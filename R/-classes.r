50796afe7d16fd041e5e01ea54c2c7224137da84
hadley <h.wickham@gmail.com>
2009-09-03 13:58:08 -0500

    Make summary suite inherit directly from suite

--- R/-classes.r
+++ R/-classes.r
@@ -2,7 +2,7 @@ if (!exists("Suite")) {
   Suite <- Object$clone()
     SuiteCase <- Suite$clone()
     ChattySuite <- Suite$clone()
-      SummarySuite <- ChattySuite$clone()
+    SummarySuite <- Suite$clone()
     StopSuite <- Suite$clone()  
     MinimalSuite <- Suite$clone()  
 }
