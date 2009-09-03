c7f898571d8c9297e55a664ff365b5200abe5531
hadley <h.wickham@gmail.com>
2009-09-03 10:12:36 -0500

    Refactor out source_dir from test_dir

--- R/test-directory.r
+++ R/test-directory.r
@@ -3,21 +3,22 @@
 #' Test files start with \code{test-} and are executed in alphabetical order 
 #' (but they shouldn't have dependencies). Helper files start with 
 #' \code{helper-} and loaded before any tests are run.
-test_dir <- function(path) {
-  tests <- dir(path, "^test-.*\\.[rR]", full.names = TRUE)
-  tests <- sort(tests)
-
-  helpers <- dir(path, "^helper-.*\\.[rR]", full.names = TRUE)
-  
+test_dir <- function(path) {  
   cur_suite <- test_suite()
   new_suite <- SummarySuite$clone()
   change_suite_to(new_suite)
   
   new_suite$start_suite()
-  lapply(helpers, source)
-  lapply(tests, source)
+  source_dir(path, "^helper-.*\\.[rR]$")
+  source_dir(path, "^test-.*\\.[rR]$")
   new_suite$end_suite()
   
   change_suite_to(cur_suite)
   invisible(new_suite)
+}
+
+source_dir <- function(path, pattern = "\\.[rR]$", chdir = TRUE) {
+  files <- sort(dir(path, pattern, full.names = TRUE))
+  
+  lapply(files, source, chdir = chdir)
 }
