71df797b33408b2e5ba5d390cd8bfadc6f6d7dbf
hadley <h.wickham@gmail.com>
2009-09-03 08:47:59 -0500

    Better documentation for stop suite

--- R/suite-stop.r
+++ R/suite-stop.r
@@ -1,9 +1,12 @@
-
-#' The default suit is executed when expect_that is run interactively, not
-#' in the framework of a test suite.  It should respond by stop()ing on errors
-#' and doing nothing otherwise. 
+#' The default suite, executed when \code{expect_that} is run interactively,
+#' or when the test files are executed by R CMD check. It responds by
+#' stop()ing on failures and doing nothing otherwise. This will ensure
+#' that a failing test suite will raise an error.
+#'
+#' This should be used when doing a quick and dirty test, or during the final
+#' automated testing of R CMD check.  Otherwise, use a suite that runs all
+#' tests and gives you more context about the problem.
 StopSuite$do({
-  
   self$add_result <- function(result) {
     if (result$passed) return()
     
