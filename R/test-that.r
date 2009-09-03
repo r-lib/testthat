e13fb1576402c7730aa1e3e8b5b2e5dfbee6f3e0
hadley <h.wickham@gmail.com>
2009-09-03 08:26:32 -0500

    Continuing to refactor error handling to improve clarity

--- R/test-that.r
+++ R/test-that.r
@@ -1,40 +1,51 @@
 test_that <- function(desc, code) {
   test_suite()$start_test(desc)
   
-  env <- new.env(parent = globalenv())
-  
-  res <- tryCatch(
-    withCallingHandlers(eval(substitute(code), env),
-      error = function(e) {
-        e$calls <- sys.calls()
-        signalCondition(e)
-      }),
-      error = function(e) e)
+  env <- new.env(parent = globalenv())  
+  res <- try_capture_stack(substitute(code), env)
   
   if (inherits(res, "error")) {
     traceback <- create_traceback(res$calls)
-    user_calls <- paste(traceback, collapse = "\n")      
-    
-    msg <- paste(as.character(res), user_calls, sep = "")
-    
+    if (length(traceback) > 0) {
+      user_calls <- paste(traceback, collapse = "\n")      
+      msg <- paste(as.character(res), user_calls, sep = "")
+    } else {
+      # Need to remove trailing newline from error message to be consistent
+      # with other messages
+      msg <- gsub("\n$", "", as.character(res))
+    }
+
     test_suite()$add_result(expectation(NA, msg))
   }
   
   test_suite()$end_test()
 }
 
+
 create_traceback <- function(callstack) {
   calls <- lapply(callstack, deparse)
   calls <- sapply(calls, paste, collapse = "\n")
   first_eval <- match("eval(expr, envir, enclos)", calls, 0)[1]
-  last <- length(calls) - 2
   
-  if (last <= first_eval) {
-    user_calls <- c()
-  } else {
-    user_calls <- calls[seq(first_eval + 1, last, by = 1)]
-    user_calls <- paste(seq_along(user_calls), ": ", user_calls, sep = "")
-    user_calls <- gsub("\n", "\n   ", user_calls)
-  }
+  if (first_eval == length(calls)) return()
+  
+  user_calls <- calls[-seq_len(first_eval)]
+  user_calls <- paste(seq_along(user_calls), ": ", user_calls, sep = "")
+  user_calls <- gsub("\n", "\n   ", user_calls)
   user_calls
+  
+}
+
+try_capture_stack <- function(quoted_code, env) {
+  capture_calls <- function(e) {
+    # Capture call stack, removing last two calls, which are added by
+    # withCallingHandlers
+    e$calls <- head(sys.calls(), -2)
+    signalCondition(e)
+  }
+  
+  tryCatch(
+    withCallingHandlers(eval(quoted_code, env), error = capture_calls),
+    error = identity
+  )
 }
