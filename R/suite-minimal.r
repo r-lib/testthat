ae35a58314f45205518c36d6bfd545ff1f75b8a5
hadley <h.wickham@gmail.com>
2009-09-03 15:54:22 -0500

    Add description of minimal test suite

--- R/suite-minimal.r
+++ R/suite-minimal.r
@@ -1,3 +1,9 @@
+#' Minimal test suite
+#' 
+#' The minimal test suite provides the absolutely minimum amount of 
+#' information: whether each expectation has succeeded, failed or experienced
+#' an error.  If you want to find out what the failures and errors actually
+#' were, you'll need to run a more informative test suite.
 MinimalSuite$do({
   self$add_result <- function(result) { 
     if (result$passed) {
