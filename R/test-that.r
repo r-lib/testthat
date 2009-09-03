a827c2461256f4921eac5e6594b54365000af348
hadley <h.wickham@gmail.com>
2009-09-02 20:47:20 -0500

    Capture call stack for errors.  Thanks Luke

--- R/test-that.r
+++ R/test-that.r
@@ -3,13 +3,27 @@ test_that <- function(desc, code) {
   
   env <- new.env(parent = globalenv())
   
-  res <- try(eval(substitute(code), env), silent = TRUE)
-  if (inherits(res, "try-error")) {
-    # trace <- paste(capture.output(traceback()), collapse = "\n")
-    # msg <- paste(as.character(res), trace, sep = "\n")
-
+  res <- tryCatch(
+    withCallingHandlers(eval(substitute(code), env),
+      error = function(e) {
+        e$calls <- sys.calls()
+        signalCondition(e)
+      }),
+      error = function(e) e)
+  
+  if (inherits(res, "error")) {
+    calls <- lapply(res$calls, deparse)
+    calls <- sapply(calls, paste, collapse = "\n")
+    first_eval <- match("eval(expr, envir, enclos)", calls)[1]
+    last_stop <- match("stop", substr(calls, 1, 4))[1]
+    
+    user_calls <- calls[seq(first_eval + 1, last_stop)]
+    user_calls <- paste(seq_along(user_calls), ": ", user_calls, sep = "")
+    user_calls <- gsub("\n", "\n   ", user_calls)
+    user_calls <- paste(user_calls, collapse = "\n")
+    
     line <- paste(rep("-", getOption("width")), collapse = "")
-    msg <- paste("\n", line, "\n",  as.character(res), line, sep = "")
+    msg <- paste("\n", line, "\n",  as.character(res), user_calls, "\n", line, sep = "")
     
     test_suite()$add_result(expectation(NA, msg))
   }
