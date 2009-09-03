6f791fa3dfb3c8b2387f87256b79093ac1e09e64
hadley <h.wickham@gmail.com>
2009-09-03 10:53:50 -0500

    A few basic tests

---
+++ tests/test-basics.r
@@ -0,0 +1,11 @@
+context("Basic tests")
+
+test_that("logical tests act as expected", {
+  expect_that(TRUE, is_true())
+  expect_that(FALSE, is_false())
+})
+
+test_that("Equality holds", {
+  expect_that(5, equals(5))
+  expect_that(10, is_identical_to(10))
+})
