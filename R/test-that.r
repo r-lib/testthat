19949823355d78839ea89a2424f05083c07d3fac
hadley <h.wickham@gmail.com>
2009-09-03 08:14:09 -0500

    Pull traceback conversion code out of test_that

--- R/test-that.r
+++ R/test-that.r
@@ -12,19 +12,8 @@ test_that <- function(desc, code) {
       error = function(e) e)
   
   if (inherits(res, "error")) {
-    calls <- lapply(res$calls, deparse)
-    calls <- sapply(calls, paste, collapse = "\n")
-    first_eval <- match("eval(expr, envir, enclos)", calls)[1]
-    last <- length(calls) - 2
-    
-    if (last <= first_eval) {
-      user_calls <- c()
-    } else {
-      user_calls <- calls[seq(first_eval + 1, last, by = 1)]
-      user_calls <- paste(seq_along(user_calls), ": ", user_calls, sep = "")
-      user_calls <- gsub("\n", "\n   ", user_calls)
-      user_calls <- paste(user_calls, collapse = "\n")      
-    }
+    traceback <- create_traceback(res$calls)
+    user_calls <- paste(traceback, collapse = "\n")      
     
     msg <- paste(as.character(res), user_calls, sep = "")
     
@@ -34,3 +23,18 @@ test_that <- function(desc, code) {
   test_suite()$end_test()
 }
 
+create_traceback <- function(callstack) {
+  calls <- lapply(callstack, deparse)
+  calls <- sapply(calls, paste, collapse = "\n")
+  first_eval <- match("eval(expr, envir, enclos)", calls, 0)[1]
+  last <- length(calls) - 2
+  
+  if (last <= first_eval) {
+    user_calls <- c()
+  } else {
+    user_calls <- calls[seq(first_eval + 1, last, by = 1)]
+    user_calls <- paste(seq_along(user_calls), ": ", user_calls, sep = "")
+    user_calls <- gsub("\n", "\n   ", user_calls)
+  }
+  user_calls
+}
