b4e5eb3ea2062b29b7f50386bd49c2b07def29b9
hadley <h.wickham@gmail.com>
2009-09-08 08:50:10 -0500

    Remove classes that are no longer used

--- R/-classes.r
+++ R/-classes.r
@@ -1,7 +1,5 @@
 if (!exists("Suite")) {
   Suite <- Object$clone()
-    SuiteCase <- Suite$clone()
-    ChattySuite <- Suite$clone()
     SummarySuite <- Suite$clone()
     StopSuite <- Suite$clone()  
     MinimalSuite <- Suite$clone()  
