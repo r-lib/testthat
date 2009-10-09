81c7ae78069b29f5650ad67b0577cd21d7df17ae
hadley <h.wickham@gmail.com>
2009-10-09 11:45:21 -0400

    Fix capitalisation

--- tests/test-basics.r
+++ tests/test-basics.r
@@ -5,7 +5,7 @@ test_that("logical tests act as expected", {
   expect_that(FALSE, is_false())
 })
 
-test_that("Equality holds", {
+test_that("equality holds", {
   expect_that(5, equals(5))
   expect_that(10, is_identical_to(10))
 })
