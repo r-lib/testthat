5eaa36a7a803ef4283434c01b43f50ee52b5b02e
hadley <h.wickham@gmail.com>
2010-02-10 23:11:31 -0600

    Use character.only in library_if_available

--- R/library.r
+++ R/library.r
@@ -14,6 +14,7 @@ library_if_available <- function(package) {
   package <- deparse(substitute(package))
   
   suppressWarnings(suppressPackageStartupMessages(
-    require(package, quietly = TRUE, warn.conflicts = FALSE)
+    require(package, quietly = TRUE, 
+      warn.conflicts = FALSE, character.only = TRUE)
   ))
 }
