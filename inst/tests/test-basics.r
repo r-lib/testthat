1a05de8149d9b66c89fd0329091194c2957f2ad6
hadley <h.wickham@gmail.com>
2010-08-24 10:25:30 -0500

    Add containment test

--- inst/tests/test-basics.r
+++ inst/tests/test-basics.r
@@ -9,3 +9,11 @@ test_that("equality holds", {
   expect_that(5, equals(5))
   expect_that(10, is_identical_to(10))
 })
+
+test_that("can't access variables from other tests 2", {
+  a <- 10
+})
+
+test_that("can't access variables from other tests 1", {
+  expect_that(exists("a"), is_false())
+})
