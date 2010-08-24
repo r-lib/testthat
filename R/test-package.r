93f0b3ea945c045d8da9f8ba96ff7bdb9b3658bd
hadley <h.wickham@gmail.com>
2010-08-24 09:51:41 -0500

    Move to new testing structure and add test_package function

---
+++ R/test-package.r
@@ -0,0 +1,9 @@
+#' Run all tests in an installed package
+#' 
+#' @param package package name
+#' @export
+#' test_package("testthat")
+test_package <- function(package) {
+  test_path <- system.file("tests", package = package)
+  test_dir(test_path, StopReporter)
+}
