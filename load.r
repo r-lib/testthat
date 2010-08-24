ac6efa4aac34a6dfc75753ba8a5528925fec5ef2
hadley <h.wickham@gmail.com>
2010-08-24 09:47:32 -0500

    Clean up and update TODO

--- load.r
+++
@@ -1,8 +0,0 @@
-l(mutatr)
-library(digest)
-library(plyr, warn.conflicts = FALSE)
-
-frame_files <- compact(llply(sys.frames(), function(x) x$ofile))
-PATH <- dirname(frame_files[[length(frame_files)]])
-
-lapply(dir(file.path(PATH, "R"), full.name=T), source)
