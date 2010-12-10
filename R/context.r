bde9d811fb3e03c260f563f88d7722a90a383576
Charlie Sharpsteen <source@sharpsteen.net>
2010-12-09 21:42:12 -0800

    Fixed context usage
    Currently the `context()` function closes the current context and starts a new
    one. There are two problems with this:
    
    - The first call to `context()` in a test file attempts to close a context when
      one has not been opened yet.
    
    - The last context opened in a test file is never closed.
    
    This patch adds three new functions `start_context(desc)`, `end_context()` and
    `next_context(desc)`.  `start_context()` should be used to open the first
    context in a test file. `next_context()` acts exactly like the current
    `context()` function and should be used to open all subsequent contexts. The
    file should end with a call to `end_context()` so that the last context is
    properly closed.
    
    `context()` has been aliased to `next_context()` in order to avoid breaking
    existing scripts.

--- R/context.r
+++ R/context.r
@@ -5,12 +5,20 @@
 #' in a single file if you so choose.
 #'
 #' @param desc description of context.  Should start with a capital letter.
-#' @export
+#' @export start_context end_context next_context context
+#' @aliases next_context context
 #' @examples
-#' context("String processing")
-#' context("Remote procedure calls")
-context <- function(desc) {
-  test_reporter()$end_context()
+#' start_context("String processing")
+#' next_context("Remote procedure calls")
+#' end_context()
+start_context <- function(desc) {
   test_reporter()$start_context(desc)
 }
-
+end_context <- function() {
+  test_reporter()$end_context()
+}
+next_context <- function(desc) {
+  end_context()
+  start_context(desc)
+}
+context <- next_context
