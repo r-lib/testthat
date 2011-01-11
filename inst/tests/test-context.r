a4434bbef291ba4a1a04052736a1b1d0a3857024
hadley <h.wickham@gmail.com>
2011-01-10 21:56:49 -0600

    Tests for context closing

---
+++ inst/tests/test-context.r
@@ -0,0 +1,36 @@
+context("Contexts")
+
+CountReporter <- Reporter$clone()$do({
+  self$context <- 0
+  self$context_count <- 0
+
+  self$test <- 0
+  self$test_count <- 0
+  
+  self$start_context <- function(desc) {
+    self$context_count <- self$context_count + 1
+    self$context <- self$context + 1
+  }
+  
+  self$end_context <- function() {
+    self$context <- self$context - 1
+    stopifnot(self$context >= 0)
+  }
+
+  self$start_test <- function(desc) {
+    self$test_count <- self$test_count + 1
+    self$test <- self$test + 1
+  }
+  
+  self$end_test <- function() {
+    self$test <- self$test - 1
+    stopifnot(self$test >= 0)
+  }
+})
+
+test_that("contexts are closed before they are opened", {
+  reporter <- CountReporter$clone()
+  test_file("context.r", reporter)
+  expect_that(report$context_count, equals(2))
+  expect_that(report$test_count, equals(2))
+})
