21b7b2e0578905e29e44d11b21f2a2c524b258c2
Paul Hiemstra <p.h.hiemstra@gmail.com>
2012-02-15 09:48:28 +0000

    Expressions are now entered using characters. This works much more robust. Also updated the docs and some other minor code changes.

--- R/generate-reference-files.r
+++ R/generate-reference-files.r
@@ -1,5 +1,5 @@
-# Load the reference file
 .get_reference = function(pattern, rdapath = "reffiles") {
+  # Load the reference file
   ref.fname = sprintf("%s/%s.rda", rdapath, pattern)
   tmp.env = new.env()
   load(ref.fname, envir = tmp.env)
@@ -8,37 +8,45 @@
   return(ref.obj)
 }
 
-# Generate a reference file in the requested location
-# given the code in expr
 .generate_ref_file = function(expr, savePrefix, rdapath = "reffiles") {  
+  # Generate a reference file in the requested location
+  # given the code in expr
   if(!file.exists(rdapath)) dir.create(rdapath)  
-  if(!inherits(expr, "{")) expr = substitute(expr)
   obj = .expr_to_list(expr)
   save(obj, file = sprintf("%s/%s.rda", rdapath, savePrefix))
 }
 
-# Evaluate the expression and return the resulting objects
-# in a list. Does this by executing the command in a new
-# environment and than loading the objects from that environment
-# into a list.
+.check_expr = function(expr) {
+  # Check if the expression has class 'character'
+  if(!inherits(expr, "character")) {
+    class_char = paste(class(expr), collapse = ", ")
+    mesg = sprintf("Expression is not of class 'character' but of class %s", class(expr))
+    stop(mesg)
+  }
+}
+
 .expr_to_list = function(expr) {
+  # Evaluate the expression and return the resulting objects
+  # in a list. Does this by executing the command in a new
+  # environment and than loading the objects from that environment
+  # into a list.
+  .check_expr(expr)
   tmp.env = new.env()
-  if(!inherits(expr, "{")) expr = substitute(expr)
-  eval(expr, envir = tmp.env)
+  eval(parse(text = expr), envir = tmp.env)
   return(.obj_from_envir(tmp.env))
 }
 
-# Extract an list of the objects present in 
-# the given environment
 .obj_from_envir = function(env.name) {  
+  # Extract an list of the objects present in 
+  # the given environment
   lsobjs = ls(envir = env.name)
   objs = lapply(lsobjs, get, envir = env.name)
   names(objs) = lsobjs
   return(objs)
 }
 
-# Return wether the reference file is missing
 .reference_missing = function(savePrefix, rdapath = "reffiles") {  
+  # Return wether the reference file is missing
   return(!file.exists(sprintf("%s/%s.rda", rdapath, savePrefix)))
 }
 
@@ -49,33 +57,39 @@
 #' returns both the result of the expression and the reference read from
 #' file to allow a test if they are equal.
 #'
-#' @param expr expression used in the test
+#' @param expr character vector with the expression used in the test, remember to escape any
+#'             quotation marks in the character expression.                
 #' @param savePrefix, prefix used in the file name that the reference is stored in
 #' @param rdapath path to the reference files
 #' @param verbose logical, if TRUE, report when a reference file is regenerated.
 #' @value a list with two parts: \code{reference} containing the reference object 
 #'        read from file, and \code{current} the freshly calculated current object.
+#' @details If you want to use quotation marks in your expression, please escape the quotes. Alternatively
+#'          mix ' and " quotes. If you want to use variables in your expression, remember to make them global using
+#'          <<-. This is not an ideal situation, but because all code is ran in separate environments, and 
+#'          evaluation is postponed to within the function \code{testthat:::.expr_to_list}, the globalization
+#'          is needed.
 #' @author Paul Hiemstra, \email{p.h.hiemstra@@gmail.com}
 #' @seealso \code{\link{matches_ref_current}}, \code{\link{clear_reference_cache}}
 #' @export
 #' @examples
 #' \dontrun{
-#' test1 = test_reference_change({a<-runif(1)
-#'                                b<-runif(2)}, savePrefix = "random")
-#' test2 = test_reference_change({a<-1
-#'                                b<-3}, savePrefix = "test")
+#' test1 = test_reference_change("{a<-runif(1)
+#'                                b<-runif(2)}", savePrefix = "random")
+#' test2 = test_reference_change("{a<-1
+#'                                b<-3}", savePrefix = "test")
 #' test_that("Reference stuff works", {
 #'   expect_that(matches_ref_current(test1), is_false())
 #'   expect_that(matches_ref_current(test2), is_true())
 #' })
 #' }
-test_reference_change = function(expr, savePrefix, rdapath = "reffiles", verbose = FALSE) {
+test_reference_change = function(expr, savePrefix, rdapath = "reffiles", verbose = FALSE) {                                                                                       
   if(.reference_missing(savePrefix, rdapath)) {
-  if(verbose) cat("r")    
-    .generate_ref_file(substitute(expr), savePrefix, rdapath)
+    if(verbose) cat("r")    
+    .generate_ref_file(expr, savePrefix, rdapath)
   }
   reference = .get_reference(savePrefix, rdapath)
-  current = .expr_to_list(substitute(expr))
+  current = .expr_to_list(expr)
   return(list(reference = reference, current = current))
 }
 
@@ -91,20 +105,18 @@ test_reference_change = function(expr, savePrefix, rdapath = "reffiles", verbose
 
 #' @export
 #' @examples
-#' expect_that(5 * 2, equals(10))
-#' expect_that(sqrt(2) ^ 2, equals(2))
 #' \dontrun{
-#' test1 = test_reference_change({a<-runif(1)
-#'                                b<-runif(2)}, savePrefix = "random")
-#' test2 = test_reference_change({a<-1
-#'                                b<-3}, savePrefix = "test")
-#' test_that("Reference stuff works", {
+#'  test1 = test_reference_change("{a<-runif(1)
+#'                                b<-runif(2)}", savePrefix = "random")
+#'  test2 = test_reference_change("{a<-1
+#'                                b<-3}", savePrefix = "test")
+#'  test_that("Reference stuff works", {
 #'   expect_that(matches_ref_current(test1), is_false())
 #'   expect_that(matches_ref_current(test2), is_true())
-#' })
+#'  })
 #' 
-#' clear_reference_cache()
-#' clear_reference_cache("random")
+#'  clear_reference_cache()
+#'  clear_reference_cache("random")
 #' }
 clear_reference_cache = function(savePrefix = NULL, rdapath = "reffiles") {
   if(is.null(savePrefix)) {
@@ -128,13 +140,11 @@ clear_reference_cache = function(savePrefix = NULL, rdapath = "reffiles") {
 
 #' @export
 #' @examples
-#' expect_that(5 * 2, equals(10))
-#' expect_that(sqrt(2) ^ 2, equals(2))
 #' \dontrun{
-#' test1 = test_reference_change({a<-runif(1)
-#'                                b<-runif(2)}, savePrefix = "random")
-#' test2 = test_reference_change({a<-1
-#'                                b<-3}, savePrefix = "test")
+#' test1 = test_reference_change("{a<-runif(1)
+#'                                b<-runif(2)}", savePrefix = "random")
+#' test2 = test_reference_change("{a<-1
+#'                                b<-3}", savePrefix = "test")
 #' test_that("Reference stuff works", {
 #'   expect_that(matches_ref_current(test1), is_false())
 #'   expect_that(matches_ref_current(test2), is_true())
@@ -143,25 +153,3 @@ clear_reference_cache = function(savePrefix = NULL, rdapath = "reffiles") {
 matches_ref_current = function(reference_object) {
   return(isTRUE(all.equal(reference_object[["reference"]], reference_object[["current"]])))
 }
-
-if(FALSE) {
-clear_reference_cache()
-
-# Some functionality tests
-test1 = test_reference_change({a<-1
-                               b<-2}, "test")
-test1 = test_reference_change({a<-1
-                               b<-2}, "test2")
-test1 = test_reference_change({a<-1
-                               b<-2}, "test3")
-matches_ref_current(test1)
-# TRUE
-
-test2 = test_reference_change({a<-1
-                             b<-3}, "test")
-matches_ref_current(test2)
-# FALSE
-
-}
-  
-  
--- inst/tests/test-reference-files.r
+++ inst/tests/test-reference-files.r
@@ -1,11 +1,38 @@
 context("Reference testing")
 # setwd("/usr/people/hiemstra/software/testthat/test_script/tests")
 
-test1 = test_reference_change({a<-runif(1)
-                               b<-runif(2)}, savePrefix = "random")
-test2 = test_reference_change({a<-1
-                               b<-3}, savePrefix = "test")
+test1 = test_reference_change("{a<-runif(1)
+                               b<-runif(2)}", savePrefix = "random")
+test2 = test_reference_change("{a<-1
+                               b<-3}", savePrefix = "test")
 test_that("Reference stuff works", {
   expect_that(matches_ref_current(test1), is_false())
   expect_that(matches_ref_current(test2), is_true())
+})
+
+# See if working with the environments is robust
+dum = function(expr) {  
+  return(test_reference_change(expr, savePrefix = "refRobust_func"))
+}
+val = test_reference_change("{a<-1
+                             b<-3}", "refRobust")
+val_fromfunc = dum("{a<-1
+           b<-3}")
+
+test_that("The expression is properly passed on through the functions", {
+  expect_that(val, equals(val_fromfunc))
+})
+
+# Check if feeding a non-character expression leads to an error
+test_that("Using a non-character expression leads to an error", {
+  expect_that(test_reference_change({a<-1
+                                     b<-3}, "refError"), throws_error())
+})
+
+# Check if escaped quotation marks pose any problem
+val = test_reference_change("{
+  a = \"spam\"}", "refEscape")
+
+test_that("Escaping quotation marks works", {
+  expect_that(matches_ref_current(val), is_true())
 })
