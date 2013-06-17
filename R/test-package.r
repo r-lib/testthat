1e62fd9af2fd8b6f0a68b805ac2d8e27cd25078a
hadley <h.wickham@gmail.com>
2013-06-17 12:53:37 -0500

    Prefer testthat subdir for package tests

--- R/test-package.r
+++ R/test-package.r
@@ -1,7 +1,8 @@
 #' Run all tests in an installed package
 #'
 #' Test are run in an environment that inherits from the package environment
-#' so that tests can access non-exported functions and variables.
+#' so that tests can access non-exported functions and variables. Tests should
+#' be placed in either \code{inst/tests}, or (better) \code{tests/testhat}.
 #'
 #' @param package package name
 #' @inheritParams test_dir
@@ -10,6 +11,11 @@
 #' \dontrun{test_package("testthat")}
 test_package <- function(package, filter = NULL, reporter = "summary") {
   test_path <- system.file("tests", package = package)
+
+  # If testthat subdir exists, use that
+  test_path2 <- file.path(test_path, "testthat")
+  if (file.exists(test_path2)) test_path <- test_path2
+
   reporter <- find_reporter(reporter)
 
   env <- new.env(parent = getNamespace(package))
