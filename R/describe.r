f9a7135fe83f511830b49fa92f2cca113773ed3c
Dirk Schumacher <mail@dirk-schumacher.net>
2014-03-02 19:58:58 +0100

    fixes indention and refactors describe()

--- R/describe.r
+++ R/describe.r
@@ -11,9 +11,9 @@
 #' @examples
 #' describe("matrix", {
 #'   it("can be multiplied by a scalar", {
-#'    m1 <- matrix(1:4, 2, 2)
-#'    m2 <- m1 * 2
-#'    expect_equivalent(matrix(1:4 * 2, 2, 2), m2)
+#'     m1 <- matrix(1:4, 2, 2)
+#'     m2 <- m1 * 2
+#'     expect_equivalent(matrix(1:4 * 2, 2, 2), m2)
 #'   })
 #'   it("can have not yet tested specs")
 #' })
@@ -29,19 +29,18 @@ describe <- function(description, code) {
   }
 
   # prepares a new environment for each it-block
-  test_describe_environment <- new.env(parent = parent.frame())
-  test_describe_environment$it <- function(it_description, it_code = NULL) {
+  describe_environment <- new.env(parent = parent.frame())
+  describe_environment$it <- function(it_description, it_code = NULL) {
     if (is_invalid_description(it_description)) {
       stop("it-description must be a string of at least length 1")
     }
-
+    if (!is.null(quote(it_code))) return()
+    
     test_description <- paste0(description, ": ", it_description)
-    if (!is.null(quote(it_code))) {
-      test_code(test_description, substitute(it_code),
-                env = test_describe_environment)
-    }
+    test_code(test_description, substitute(it_code),
+                env = describe_environment)
   }
 
-  eval(substitute(code), test_describe_environment)
+  eval(substitute(code), describe_environment)
   invisible()
 }
