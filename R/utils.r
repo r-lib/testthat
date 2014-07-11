3af0f099afe4f7884e90527a4eaa97ca697e55f5
Jack Wasey <jack@jackwasey.com>
2014-07-11 06:30:49 -0400

    include lsf function to list functions in a package.

--- R/utils.r
+++ R/utils.r
@@ -11,3 +11,18 @@ grepl2 <- function(pattern, x, ...) {
 
   vapply(pattern, grepl, x, ..., FUN.VALUE = logical(1), USE.NAMES = FALSE)
 }
+
+
+#' @title list all functions in a package
+#' @param pkg character string containing package name
+#' @return character vector of functions in given package
+lsf <- function(pkg) {
+  envirName <- paste("package", pkg, sep = ":")
+  everything <- ls(pos = envirName, all.names = TRUE)
+  funcs <- c()
+  for (e in everything) {
+    if (is.function(get(e, envir = as.environment(envirName), inherits = F)))
+      funcs <- append(funcs, e)
+  }
+  funcs
+}
