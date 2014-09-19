3f18bde96e364d8074fd0a179f7e6e9c566728dd
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-09-19 18:37:14 +0200

    reformat error message

--- src/reassign.c
+++ src/reassign.c
@@ -8,8 +8,8 @@ SEXP reassign_function(SEXP name, SEXP env, SEXP old_fun, SEXP new_fun)
 {
   if (TYPEOF(name) != SYMSXP) error("name must be a symbol");
   if (TYPEOF(env) != ENVSXP) error("env must be an environment");
+  if (TYPEOF(old_fun) != CLOSXP) error("old_fun must be a function");
   if (TYPEOF(new_fun) != CLOSXP) error("new_fun must be a function");
-  if (TYPEOF(old_fun) != CLOSXP) error("Value to replace must be a function");
 
   FORMALS(old_fun) = FORMALS(new_fun);
   BODY(old_fun) = BODY(new_fun);
