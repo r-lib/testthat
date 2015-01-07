ffed0cb73f1656bea910c0f84b74e0a2185eccd8
RobertZK <technoguyrob@gmail.com>
2015-01-07 16:28:56 -0600

    give warning on fg and bg parameter usage

--- R/colour-text.r
+++ R/colour-text.r
@@ -6,9 +6,12 @@ testthat_colours <- list(
 )
 
 #' @export
-colourise <- function(text, as = c("passed", "skipped", "error")) {
+colourise <- function(text, as = c("passed", "skipped", "error"), fg, bg) {
+  if (!missing(fg)) warning("The fg parameter to testthat::colourise is deprecated.", call. = FALSE)
+  if (!missing(bg)) warning("The bg parameter to testthat::colourise is deprecated.", call. = FALSE)
   colour_config <- getOption("testthat.use_colours", TRUE)
   if (!isTRUE(colour_config)) return(text)
   as <- match.arg(as)
   testthat_colours[[as]](text)
 }
+
