61253b43b2cd60dfbe0dafd3ee5b02f469049e32
RobertZK <technoguyrob@gmail.com>
2015-01-07 12:23:03 -0600

    export colourise

--- R/colour-text.r
+++ R/colour-text.r
@@ -5,6 +5,7 @@ testthat_colours <- list(
   error = crayon::red
 )
 
+#' @export
 colourise <- function(text, as = c("passed", "skipped", "error")) {
   colour_config <- getOption("testthat.use_colours", TRUE)
   if (!isTRUE(colour_config)) return(text)
