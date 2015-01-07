177537270ccca2468f6ab614827652d9b98a94e4
RobertZK <technoguyrob@gmail.com>
2015-01-07 16:46:59 -0600

    ignore foreground argument

--- R/colour-text.r
+++ R/colour-text.r
@@ -1,3 +1,6 @@
+allowed_colors <- c("black", "blue", "brown", "cyan", "dark gray",
+  "green", "light blue", "light cyan", "light gray", "light green",
+  "light purple", "light red", "purple", "red", "white", "yellow")
 
 testthat_colours <- list(
   passed = crayon::green,
@@ -7,8 +10,18 @@ testthat_colours <- list(
 
 #' @export
 colourise <- function(text, as = c("passed", "skipped", "error"), fg, bg) {
-  if (!missing(fg)) warning("The fg parameter to testthat::colourise is deprecated.", call. = FALSE)
-  if (!missing(bg)) warning("The bg parameter to testthat::colourise is deprecated.", call. = FALSE)
+  fg_warn <- function() warning("The fg parameter to testthat::colourise is deprecated.", call. = FALSE)
+  if (any(is.element(as, allowed_colors))) {
+    fg_warn()
+    return(colourise(text))
+  } else if (!missing(fg)) {
+    fg_warn()
+  }
+  
+  if (!missing(bg)) {
+    warning("The bg parameter to testthat::colourise is deprecated.", call. = FALSE)
+  }
+
   colour_config <- getOption("testthat.use_colours", TRUE)
   if (!isTRUE(colour_config)) return(text)
   as <- match.arg(as)
