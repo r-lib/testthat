f1604398a0b484f0f2503dc3fb4028ce35bf5241
Kevin Ushey <kevinushey@gmail.com>
2015-01-18 23:38:57 -0800

    update NEWS; various tweaks

--- tests/testthat/cpp/test-cpp.cpp
+++ tests/testthat/cpp/test-cpp.cpp
@@ -1,6 +1,9 @@
 #include <tests/testthat.h>
 
-test_that("2 + 2 == 4")
-{
-  expect_true((2 + 2) == 4);
+int twoPlusTwo() { return 2 + 2; }
+
+context("Example Unit Test") {
+  test_that("2 + 2 == 4") {
+    expect_true(twoPlusTwo() == 4);
+  }
 }
