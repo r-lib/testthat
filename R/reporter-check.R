6fa8711e0f77f232518fb0fc3586123eece2e3d0
Vitalie Spinu <spinuvit@gmail.com>
2015-04-23 18:06:36 +0200

    Make failure location syntax consistent with R reporting

--- R/reporter-check.R
+++ R/reporter-check.R
@@ -96,7 +96,7 @@ failure_header <- function(x) {
   if (is.null(ref)) {
     location <- ""
   } else {
-    location <- paste0(" (@", attr(ref, "srcfile")$filename, "#", ref[1], ")")
+    location <- paste0(" (at ", attr(ref, "srcfile")$filename, "#", ref[1], ")")
   }
 
   paste0(type, location, ": ", x$test, " ")
