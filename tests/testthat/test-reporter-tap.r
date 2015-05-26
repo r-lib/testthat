426df1566e1f9288ed3379198f935bb75251d8dc
Neal Richardson <neal@crunch.io>
2015-05-26 16:40:23 -0700

    Get the right case

--- tests/testthat/test-reporter-tap.r
+++ tests/testthat/test-reporter-tap.r
@@ -10,5 +10,5 @@ test_that("TAP reporter handles context and pass/fail/skip", {
   expect_identical(tap.report[2], "# Context Bare ")
   expect_true("ok 7 equality holds " %in% tap.report)
   expect_true("not ok 10 empty test with error " %in% tap.report)
-  expect_true("ok 24 # skip Skipping to avoid certain failure " %in% tap.report)
+  expect_true("ok 24 # SKIP Skipping to avoid certain failure " %in% tap.report)
 })
