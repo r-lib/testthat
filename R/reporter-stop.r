2491a2de56aa25f2b8e7a25eacfecf2e87528c3d
Kirill Müller <krlmlr@users.noreply.github.com>
2015-06-01 22:46:05 +0200

    R CMD check now uses CheckReporter

--- R/reporter-stop.r
+++ R/reporter-stop.r
@@ -4,7 +4,7 @@ NULL
 #' Test reporter: stop on error.
 #'
 #' The default reporter, executed when \code{expect_that} is run
-#' interactively, or when the test files are executed by R CMD check. It
+#' interactively. It
 #' responds by \link{stop}()ing on failures and doing nothing otherwise. This
 #' will ensure that a failing test will raise an error.
 #'
