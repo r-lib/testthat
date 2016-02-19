751ff7437e6a71bb1af49ad95380ca9f1749b493
hadley <h.wickham@gmail.com>
2016-02-20 06:38:53 +1100

    Tweak bare expectations test

--- tests/testthat/test-bare.r
+++ tests/testthat/test-bare.r
@@ -1,17 +1,8 @@
 context("Bare expectations")
 
-expect_that(1, equals(1))
 expect_equal(2, 2)
-
-expect_that(2, is_less_than(3))
-expect_that(3, is_more_than(2))
-
-expect_more_than(3, 2)
-expect_gt(3, 2)
-expect_gte(3, 3)
-expect_less_than(2, 3)
-expect_lt(2, 3)
-expect_lte(2, 2)
+expect_true(TRUE)
+expect_error(stop("!"))
 
 expect_error(expect_false(FALSE), NA)
 expect_error(expect_false(TRUE))
