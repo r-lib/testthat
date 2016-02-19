a2cc69c45691d9c67c06bb814a1d64bce81f90fe
hadley <h.wickham@gmail.com>
2016-02-19 17:08:31 +1100

    Add {} for covr

--- R/compare-time.R
+++ R/compare-time.R
@@ -1,19 +1,23 @@
 #' @rdname compare
 #' @export
 compare.POSIXt <- function(x, y, ..., max_diffs = 9) {
-  if (identical(x, y))
+  if (identical(x, y)) {
     return(no_difference())
+  }
 
-  if (!inherits(y, "POSIXt"))
+  if (!inherits(y, "POSIXt")) {
     return(diff_class(x, y))
-  if (!same_length(x, y))
+  }
+  if (!same_length(x, y)) {
     return(diff_length(x, y))
+  }
 
   x <- standardise_tzone(as.POSIXct(x))
   y <- standardise_tzone(as.POSIXct(y))
 
-  if (!same_attr(x, y))
+  if (!same_attr(x, y)) {
     return(diff_attr(x, y))
+  }
 
   diff <- !vector_equal(x, y)
 
