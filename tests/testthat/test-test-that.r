cfd04899fd8f3908d5b389f804038f1a05e460c3
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-02-19 10:40:59 +0100

    move test

--- tests/testthat/test-test-that.r
+++ tests/testthat/test-test-that.r
@@ -134,3 +134,25 @@ test_that("line numbers are found and given to reporters", {
   "
   expect_equal(.test_and_fetch_lines(code), 3)
 })
+
+test_that("return value from test_that", {
+  with_reporter("", success <- test_that("success", {}))
+  expect_true(success)
+  with_reporter("", success <- test_that("success", succeed()))
+  expect_true(success)
+  with_reporter("", success <- test_that("success", expect(TRUE, "Yes!")))
+  expect_true(success)
+
+  with_reporter("", error <- test_that("error", barf))
+  expect_false(error)
+
+  with_reporter("", failure <- test_that("failure", expect_true(FALSE)))
+  expect_false(failure)
+  with_reporter("", failure <- test_that("failure", fail()))
+  expect_false(failure)
+  with_reporter("", success <- test_that("failure", expect(FALSE, "No!")))
+  expect_false(failure)
+
+  with_reporter("", skip <- test_that("skip", skip("skipping")))
+  expect_false(skip)
+})
