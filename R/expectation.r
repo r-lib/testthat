05a0aefc9729379997b4c616ea4e3bd25f166029
hadley <h.wickham@gmail.com>
2016-02-21 07:03:41 +1100

    Eliminate expectations file

--- R/expectation.r
+++ R/expectation.r
@@ -43,6 +43,33 @@ expect <- function(exp, ...) {
   invisible(exp)
 }
 
+make_label <- function(object, info = NULL, label = NULL) {
+  if (!is.null(info)) {
+    warning("`info` has been deprecated.", call. = FALSE)
+  }
+  if (!is.null(label)) {
+    warning("`label` has been deprecated.", call. = FALSE)
+  }
+
+  label(object)
+}
+
+label <- function(obj) {
+  x <- lazyeval::lazy(obj)$expr
+
+  if (is.atomic(x)) {
+    format(x)
+  } else if (is.name(x)) {
+    paste0("`", as.character(x), "`")
+  } else {
+    chr <- deparse(x)
+    if (length(chr) > 1) {
+      chr <- paste0(chr[1], "...")
+    }
+    chr
+  }
+}
+
 expectation_type <- function(exp) {
   class(exp)[[which(class(exp) == "expectation") + 1L]]
 }
