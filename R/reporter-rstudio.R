b0fefc95f1787118de11214a3656563382a0820a
hadley <h.wickham@gmail.com>
2016-02-29 10:42:20 -0600

    Refactor RStudio reporter

--- R/reporter-rstudio.R
+++ R/reporter-rstudio.R
@@ -19,20 +19,6 @@ RstudioReporter <- setRefClass("RstudioReporter", contains = "Reporter",
       if (expectation_success(result))
         return()
 
-      if (expectation_skip(result)) {
-        status <- "info"
-        prefix <- "Skipped"
-      } else if (expectation_failure(result)) {
-        status <- "error"
-        prefix <- "Failed"
-      } else if (expectation_error(result)) {
-        status <- "error"
-        prefix <- "Errored"
-      } else {
-        status <- expectation_type(result)
-        prefix <- "Other"
-      }
-
       ref <- result$srcref
       if (is.null(ref)) {
         location <- "?#?:?"
@@ -40,8 +26,10 @@ RstudioReporter <- setRefClass("RstudioReporter", contains = "Reporter",
         location <- paste0(attr(ref, "srcfile")$filename, "#", ref[1], ":1")
       }
 
-      cat(location, " [", status, "] ", test, ". ", strsplit(result$message, "\n")[[1]][1], "\n",
-        sep = "")
+      status <- expectation_type(result)
+      first_line <- strsplit(result$message, "\n")[[1]][1]
+
+      cat(location, " [", status, "] ", test, ". ", first_line, "\n", sep = "")
     }
   )
 )
