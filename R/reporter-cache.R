7d2b45a874e0b2309726250eae5db2c033cf0ade
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-03-01 20:20:38 +0100

    new CacheReporter

---
+++ R/reporter-cache.R
@@ -0,0 +1,35 @@
+#' @include reporter.R
+NULL
+
+#' Cache reporter: Defer reporting to the current reporters until the end
+#'
+#' Active sinks also affect reporter output.  This reporter collects all
+#' expectations, and forwards them to the reporter that was active when it was
+#' created.
+#'
+#' @keywords internal
+CacheReporter <- R6::R6Class(
+  "CacheReporter", inherit = Reporter,
+
+  public = list(
+    initialize = function(reporter = get_reporter()) {
+      private$.args <- Stack$new()
+      private$.reporter <- reporter
+    },
+
+    add_result = function(context, test, result) {
+      private$.args$push(list(context = context, test = test, result = result))
+    },
+
+    end_reporter = function() {
+      for (args in private$.args$as_list()) {
+        do.call(private$.reporter$add_result, args)
+      }
+    }
+  ),
+
+  private = list(
+    .args = NULL,
+    .reporter = NULL
+  )
+)
