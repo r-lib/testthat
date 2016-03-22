649aa7e14fa237f94c37fb2e3328db061b6a3e6f
hadley <h.wickham@gmail.com>
2016-03-22 16:04:23 -0500

    Tweak rule()

--- R/utils.R
+++ R/utils.R
@@ -19,10 +19,11 @@ rule <- function(..., pad = "-") {
   if (nargs() == 0) {
     title <- ""
   } else {
-    title <- paste0(...)
+    title <- paste0(..., " ")
   }
-  width <- getOption("width") - nchar(title) - 5
-  cat(title, " ", paste(rep(pad, width, collapse = "")), "\n", sep = "")
+  width <- getOption("width") - nchar(title)
+
+  cat(title, paste(rep(pad, width, collapse = "")), "\n", sep = "")
 }
 
 
