fa7a27b7b89900667a8126a8b00ce3523aa4e661
Neal Richardson <neal@crunch.io>
2016-05-03 10:07:30 -0700

    Make TAP reporter tests run, and update assertions so that they pass

--- tests/testthat/test-reporter-tap.R
+++ tests/testthat/test-reporter-tap.R
@@ -1,10 +1,11 @@
 context("TAP reporter")
 
+tap.report <- capture.output(test_dir("test_dir", reporter = "tap"))
+
 test_that("TAP reporter handles context and pass/fail/skip", {
-  tap.report <- capture.output(test_dir("test_dir", reporter = "tap"))
-  expect_identical(tap.report[1], "1..24")
-  expect_identical(tap.report[2], "# Context Bare ")
-  expect_true("ok 7 equality holds " %in% tap.report)
-  expect_true("not ok 10 empty test with error " %in% tap.report)
-  expect_true("ok 24 # SKIP Skipping to avoid certain failure " %in% tap.report)
+  expect_identical(tap.report[1], "1..22")
+  expect_identical(tap.report[2], "# Context Basic")
+  expect_true("ok 6 equality holds" %in% tap.report)
+  expect_true("not ok 8 empty test with error" %in% tap.report)
+  expect_true("ok 22 # SKIP Skipping to avoid certain failure" %in% tap.report)
 })
