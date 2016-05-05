527c53d60c651e714dc968922a3dfa324c37d95a
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-05-06 01:13:42 +0200

    no special handling for stop() needed

--- R/reporter-debug.R
+++ R/reporter-debug.R
@@ -32,32 +32,15 @@ recover <- function(start_frame = 1L, end_frame = sys.nframe())
     tState <- tracingState(FALSE)
     on.exit(tracingState(tState))
   }
-  from <- 0L
-  frame_range <- seq.int(from = end_frame, to = start_frame, by = -1L)
-  for (i in frame_range) {
-    calli <- calls[[i]]
-    fname <- calli[[1L]]
-    if (!is.name(fname) || is.na(match(as.character(fname),
-                                       c("recover", "stop", "Stop")))) {
-      from <- i
-      break
-    }
+  from <- end_frame
+
+  calls <- limitedLabels(calls[start_frame:from])
+  repeat {
+    which <- menu(calls, title = "\nEnter a frame number, or 0 to exit  ")
+    if (which)
+      eval(substitute(browser(skipCalls = skip),
+                      list(skip = 7 - which)),
+           envir = sys.frame(start_frame - 2 + which))
+    else break
   }
-  if (from > 0L) {
-    if (!interactive()) {
-      try(dump.frames())
-      cat(gettext("recover called non-interactively; frames dumped, use debugger() to view\n"))
-      return(NULL)
-    }
-    calls <- limitedLabels(calls[start_frame:from])
-    repeat {
-      which <- menu(calls, title = "\nEnter a frame number, or 0 to exit  ")
-      if (which)
-        eval(substitute(browser(skipCalls = skip),
-                        list(skip = 7 - which)),
-             envir = sys.frame(start_frame - 2 + which))
-      else break
-    }
-  }
-  else cat(gettext("No suitable frames for recover()\n"))
 }
