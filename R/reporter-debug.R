e875c1c9260a2989c1e70ce5095c778d167ea784
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-05-06 03:06:16 +0200

    also test browser entry points
    - mock browse_frame() which is called from our recover()
    - record the frame that would have been used for the browser() call
    - list variables defined in that frame, compare with expectation

--- R/reporter-debug.R
+++ R/reporter-debug.R
@@ -37,10 +37,15 @@ recover <- function(start_frame = 1L, end_frame = sys.nframe())
   calls <- limitedLabels(calls[start_frame:from])
   repeat {
     which <- menu(calls, title = "\nEnter a frame number, or 0 to exit  ")
-    if (which)
-      eval(substitute(browser(skipCalls = skip),
-                      list(skip = 7 - which)),
-           envir = sys.frame(start_frame - 2 + which))
+    if (which) {
+      frame <- sys.frame(start_frame - 2 + which)
+      browse_frame(frame, skip = 7 - which)
+    }
     else break
   }
+}
+
+browse_frame <- function(frame, skip) {
+  eval(substitute(browser(skipCalls = skip), list(skip = skip)),
+       envir = frame)
 }
