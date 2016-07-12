d629cc22cb81fe9f164c17fd103336461c5cfcbd
Lukasz A. Bartnik <lukaszb@microsoft.com>
2016-07-12 14:14:13 -0700

    fix junit reporter test

--- tests/testthat/test-reporter-junit.R
+++ tests/testthat/test-reporter-junit.R
@@ -5,11 +5,12 @@ test_that("Junit reporter regression", {
   junit.reporter     <- JunitReporter$new(file = output.file)
   junit.reporter$out <- textConnection(NULL, "w")
 
+  # test_dir("tests/testthat/test_dir", reporter = junit.reporter)
   test_dir("test_dir", reporter = junit.reporter)
   junit.report <- textConnectionValue(junit.reporter$out)
 
   # standard output
-  expect_identical(junit.report[1], "Bare : ")
+  expect_identical(junit.report[1], "Bare : ..")
   expect_identical(junit.report[2], "Basic : .......")
   expect_identical(junit.report[3], "empty : E")
   expect_identical(junit.report[4], "error : E.EFEE.")
@@ -18,17 +19,20 @@ test_that("Junit reporter regression", {
   expect_identical(junit.report[7], "skip : .")
 
   # XML file output
-  testsuite <- function(tests, failures, errors, name) {
-    paste0('<testsuite tests="', tests, '" failures="', failures,
-           '" errors="', errors, '" name="', name, '"')
+  testsuite_in_report <- function(tests, failures, errors, name) {
+    line <- paste0('<testsuite tests="', tests,
+                   '" failures="', failures,
+                   '" errors="', errors,
+                   '" name="', name, '"')
+    length(grep(line, xml.report)) > 0
   }
 
   xml.report <- textConnectionValue(output.file)
-  expect_true(testsuite(7, 0, 0, "Basic") %in% xml.report)
-  expect_true(testsuite(1, 0, 1, "empty") %in% xml.report)
-  expect_true(testsuite(7, 1, 4, "error") %in% xml.report)
-  expect_true(testsuite(5, 2, 0, "failures") %in% xml.report)
-  expect_true(testsuite(1, 0, 0, "helper") %in% xml.report)
-  expect_true(testsuite(1, 1, 0, "skip") %in% xml.report)
+  expect_true(testsuite_in_report(7, 0, 0, "Basic"))
+  expect_true(testsuite_in_report(1, 0, 1, "empty"))
+  expect_true(testsuite_in_report(7, 1, 4, "error"))
+  expect_true(testsuite_in_report(5, 2, 0, "failures"))
+  expect_true(testsuite_in_report(1, 0, 0, "helper"))
+  expect_true(testsuite_in_report(1, 1, 0, "skip"))
 })
 
