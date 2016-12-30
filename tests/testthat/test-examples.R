6823d4d5f1724cd9b8384a6bdfe4698de77c19f1
Jim Hester <james.f.hester@gmail.com>
2016-12-30 13:34:59 -0500

    Add simple test for running examples

---
+++ tests/testthat/test-examples.R
@@ -0,0 +1,13 @@
+context("test_examples")
+
+test_that("test_examples works with installed packages", {
+  env_test$package <- "testthat"
+  on.exit(env_test$package <- NULL)
+  with_mock(
+    test_rd = identity,
+    {
+      res <- test_examples()
+
+    })
+  expect_true(length(res) > 1)
+})
