a44d313f6664ced787bf0447edfbfe1f3a1dd0ca
Neal Richardson <neal@crunch.io>
2017-01-23 21:43:28 -0800

    Extend expect_length to use an S4 object's length method, if exists

--- R/expect-length.R
+++ R/expect-length.R
@@ -15,8 +15,8 @@ expect_length <- function(object, n) {
   stopifnot(is.numeric(n), length(n) == 1)
   lab <- label(object)
 
-  if (!is_vector(object)) {
-    fail(sprintf("%s is not a vector.", lab))
+  if (!has_length(object)) {
+    fail(sprintf("%s does not have a defined length.", lab))
   }
 
   expect(
@@ -27,6 +27,12 @@ expect_length <- function(object, n) {
   invisible(object)
 }
 
-is_vector <- function(x) {
-  typeof(x) %in% c("logical", "integer", "double", "complex", "character", "raw", "list")
+#' @importFrom methods selectMethod
+has_length <- function(x) {
+  # We can assert the length of objects that either are/inherit from base R
+  # classes that have length defined, or are S4 objects that have a length
+  # method defined. For an S4 object without a length method, the .Primitive
+  # length function will always return 1, which could be misleading.
+  typeof(x) %in% c("logical", "integer", "double", "complex", "character", "raw", "list") ||
+    (isS4(x) && !identical(selectMethod(length, class(x)), length))
 }
--- tests/testthat/test-expect-length.R
+++ tests/testthat/test-expect-length.R
@@ -1,12 +1,38 @@
 context("expect_length")
 
-test_that("fails if not a vector", {
-  expect_failure(expect_length(environment(), 1), "not a vector")
+test_that("fails if not a vector or object with defined length method", {
+  expect_failure(expect_length(environment(), 1),
+    "does not have a defined length")
 })
 
 test_that("length computed correctly", {
   expect_success(expect_length(1, 1))
-  expect_failure(expect_length(1, 2))
+  expect_failure(expect_length(1, 2), "has length 1, not length 2.")
+  expect_success(expect_length(1:10, 10))
+  expect_success(expect_length(letters[1:5], 5))
+})
+
+test_that("uses S4 length method, if exists", {
+  # A has no length method defined
+  A <- setClass("ExpectLengthA", slots=c(x="numeric"))
+  expect_failure(expect_length(A(x=1:5), 5),
+    "does not have a defined length")
+  # Even though 'length' does not fail on an A
+  expect_identical(length(A(x=1:5)), 1L)
+
+  # B does has a length method defined
+  B <- setClass("ExpectLengthB", slots=c(x="numeric"))
+  setMethod("length", "ExpectLengthB", function (x) 5L)
+  expect_success(expect_length(B(x=1:8), 5))
+
+  # C does not, but it inherits from something that does
+  C <- setClass("ExpectLengthC", contains="list")
+  expect_success(expect_length(C(), 0))
+  expect_success(expect_length(C(1:10), 10))
+
+  # D does not explicitly have one, but it inherits from B, which does
+  D <- setClass("ExpectLengthD", contains="ExpectLengthB")
+  expect_success(expect_length(D(x=1:8), 5))
 })
 
 test_that("returns input", {
