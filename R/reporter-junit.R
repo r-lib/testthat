40f689edcec7a26df8150d66d44e32799866bfcf
Alex Bertram <alex@bedatadriven.com>
2017-02-17 21:51:11 +0100

    Allow configuration of test_check() using options()
     This adds two options:
        * testthat.default_check_reporter: allows configuration of a different reporter
          to use for test_check(). For example, 'junit'
        * testthat.junit.output_file: specifies a file to which the junit xml should
          be written.
    
    These are needed so that Ci systems can run test scripts in R packages
    without modification, but first set these options() so that test output will
    be written to a known location where it can be read and parsed.

--- R/reporter-junit.R
+++ R/reporter-junit.R
@@ -117,7 +117,11 @@ JunitReporter <- R6::R6Class("JunitReporter", inherit = Reporter,
     },
 
     end_reporter = function() {
-      if (inherits(self$out, "connection")) {
+      output_file <- getOption("testthat.junit.output_file")
+
+      if (!is.null(output_file)) {
+        xml2::write_xml(self$doc, output_file, format = TRUE)
+      } else if (inherits(self$out, "connection")) {
         file <- tempfile()
         xml2::write_xml(self$doc, file, format = TRUE)
         writeLines(readLines(file), self$out)
