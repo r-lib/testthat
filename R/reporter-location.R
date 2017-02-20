f04fe4e4939825e6e913f233b0836c1c48b8d7bf
Kirill Müller <krlmlr+r@mailbox.org>
2017-02-20 17:08:24 +0100

    remove extra newline

--- R/reporter-location.R
+++ R/reporter-location.R
@@ -30,10 +30,6 @@ LocationReporter <- R6::R6Class("LocationReporter", inherit = Reporter,
     end_test = function(context, test) {
       self$cat_line("End test: ", test)
       self$cat_line()
-    },
-
-    end_reporter = function() {
-      self$cat_line()
     }
   )
 )
