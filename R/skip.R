7ac8d4ad91319f4ee81dba9ea10ecb8499ee5fd7
brodieG <brodieG@users.noreply.github.com>
2017-04-09 14:31:05 -0400

    dummy commit to kick travis off again
    Looks like the error might be of the type that
    only happens randomly; trying to see if this is
    true
    https://travis-ci.org/hadley/testthat/builds/220275163

--- R/skip.R
+++ R/skip.R
@@ -41,7 +41,7 @@
 #' ## run with `test_file`, `test_dir`, `test_check`, etc.
 #'
 #' test_that("skip example", {
-#'   expect_equal(1, 1L)     # this expectation runs
+#'   expect_equal(1, 1L)    # this expectation runs
 #'   skip('skip')
 #'   expect_equal(1, 2)     # this one skipped
 #'   expect_equal(1, 3)     # this one is also skipped
