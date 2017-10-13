b7f91176bf72df4021baecb05eb119606c184121
Kevin Ushey <kevinushey@gmail.com>
2017-10-13 07:42:27 -0700

    add simple tests

---
+++ tests/testthat/test-catch.R
@@ -0,0 +1,10 @@
+context("Catch")
+
+test_that("get_routine() finds own 'run_testthat_tests'", {
+  routine <- get_routine("testthat", "run_testthat_tests")
+  expect_is(routine, "NativeSymbolInfo")
+})
+
+test_that("get_routine() fails when no routine exists", {
+  expect_error(get_routine("utils", "no_such_routine"))
+})
