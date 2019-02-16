eda1cefd4b3132a629dcf132b767e84121caffb0
Patrick Miller <pmiller@civisanalytics.com>
2019-02-15 22:10:08 -0600

    TST add more expect-self-test tests

--- tests/testthat/test-expect-self-test.R
+++ tests/testthat/test-expect-self-test.R
@@ -8,3 +8,24 @@ test_that("fail always fails", {
 test_that("succeed always succeeds", {
   expect_success(succeed())
 })
+
+test_that("expect_success errors if null", {
+  expect_error(expect_success(NULL))
+})
+
+test_that("expect_success errors with msg", {
+  expect_error(expect_success(stop("asdf")), 'asdf')
+})
+
+test_that("expect_failure errors if null", {
+  expect_error(expect_failure(NULL))
+})
+
+test_that("expect_failure errors if no failure", {
+  expect_error(expect_failure(TRUE))
+})
+
+test_that("show_failure", {
+  expect_null(show_failure(NULL))
+  expect_output(show_failure(expect_true(FALSE)), "FALSE isn't true.")
+})
