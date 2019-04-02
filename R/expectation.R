d51a8b6c302984b9a2a28c80d59168fba90afe6a
Hadley Wickham <h.wickham@gmail.com>
2019-04-02 07:49:06 -0500

    More expectation object tidying/refactoring

--- R/expectation.R
+++ R/expectation.R
@@ -1,15 +1,16 @@
 #' The building block of all `expect_` functions
 #'
-#' Use this if you are writing your own expectation. See
+#' Call this function when writing your own expectations. See
 #' `vignette("custom-expectation")` for details.
 #'
-#' @param ok Was the expectation successful?
-#' @param failure_message What message should be shown if the expectation was
-#'   not successful?
-#' @param info Additional information. Included for backward compatibility
-#'   only and new expectations should not use it.
-#' @param srcref Only needed in very rare circumstances where you need to
-#'   forward a srcref captured elsewhere.
+#' @param ok `TRUE` or `FALSE` indicating if the expectation was successful.
+#' @param failure_message Message to show if the expectation failed.
+#' @param info Character vector continuing additional information. Included
+#'   for backward compatibility only and new expectations should not use it.
+#' @param srcref Location of the failure. Should only needed to be explicitly
+#'   supplied when you need to forward a srcref captured elsewhere.
+#' @return An expectation object. Signals the expectation condition
+#'   with a `continue_test` restart.
 #' @export
 expect <- function(ok, failure_message, info = NULL, srcref = NULL) {
   type <- if (ok) "success" else "failure"
@@ -17,11 +18,7 @@ expect <- function(ok, failure_message, info = NULL, srcref = NULL) {
   exp <- expectation(type, message, srcref = srcref)
 
   withRestarts(
-    if (expectation_broken(exp)) {
-      stop(exp)
-    } else {
-      signalCondition(exp)
-    },
+    if (ok) signalCondition(exp) else stop(exp),
     continue_test = function(e) NULL
   )
 
@@ -31,7 +28,7 @@ expect <- function(ok, failure_message, info = NULL, srcref = NULL) {
 #' Construct an expectation object
 #'
 #' For advanced use only. If you are creating your own expectation, you should
-#' call `expect()` instead. See `vignette("custom-expectation")` for more
+#' call [expect()] instead. See `vignette("custom-expectation")` for more
 #' details.
 #'
 #' @param type Expectation type. Must be one of "success", "failure", "error",
@@ -63,89 +60,6 @@ expectation <- function(type, message, srcref = NULL) {
 #' @param x object to test for class membership
 is.expectation <- function(x) inherits(x, "expectation")
 
-expectation_type <- function(exp) {
-  stopifnot(is.expectation(exp))
-  gsub("^expectation_", "", class(exp)[[1]])
-}
-
-expectation_success <- function(exp) {
-  expectation_type(exp) == "success"
-}
-
-expectation_failure <- function(exp) {
-  expectation_type(exp) == "failure"
-}
-
-expectation_error <- function(exp) {
-  expectation_type(exp) == "error"
-}
-
-expectation_skip <- function(exp) {
-  expectation_type(exp) == "skip"
-}
-
-expectation_warning <- function(exp) {
-  expectation_type(exp) == "warning"
-}
-
-expectation_broken <- function(exp) {
-  expectation_failure(exp) || expectation_error(exp)
-}
-expectation_ok <- function(exp) {
-  expectation_type(exp) %in% c("success", "warning")
-}
-
-
-
-as.expectation <- function(x, ...) UseMethod("as.expectation", x)
-
-#' @export
-as.expectation.default <- function(x, ..., srcref = NULL) {
-  stop(
-    "Don't know how to convert '", paste(class(x), collapse = "', '"),
-    "' to expectation.", call. = FALSE
-  )
-}
-
-#' @export
-as.expectation.expectation <- function(x, ..., srcref = NULL) {
-  if (is.null(x$srcref)) {
-    x$srcref <- srcref
-  }
-  x
-}
-
-#' @export
-as.expectation.error <- function(x, ..., srcref = NULL) {
-  error <- x$message
-
-  msg <- gsub("Error.*?: ", "", as.character(error))
-
-  # Need to remove trailing newline from error message to be consistent
-  # with other messages
-  msg <- gsub("\n$", "", msg)
-
-  expectation("error", msg, srcref)
-}
-
-#' @export
-as.expectation.warning <- function(x, ..., srcref = NULL) {
-  msg <- x$message
-
-  # msg <- gsub("Error.*?: ", "", as.character(error))
-  # msg <- gsub("\n$", "", msg)
-
-  expectation("warning", msg, srcref)
-}
-
-#' @export
-as.expectation.skip <- function(x, ..., srcref = NULL) {
-  error <- x$message
-  msg <- gsub("Error.*?: ", "", as.character(error))
-
-  expectation("skip", msg, srcref)
-}
-
 #' @export
 print.expectation <- function(x, ...) {
   cat(format(x), "\n")
@@ -165,6 +79,60 @@ format.expectation_error <- function(x, ...) {
 format.expectation <- function(x, ...) {
   x$message
 }
+
+# as.expectation ----------------------------------------------------------
+
+as.expectation <- function(x, ...) UseMethod("as.expectation", x)
+
+#' @export
+as.expectation.default <- function(x, ..., srcref = NULL) {
+  stop(
+    "Don't know how to convert '", paste(class(x), collapse = "', '"),
+    "' to expectation.", call. = FALSE
+  )
+}
+
+#' @export
+as.expectation.expectation <- function(x, ..., srcref = NULL) {
+  x$srcref <- x$srcref %||% srcref
+  x
+}
+
+#' @export
+as.expectation.error <- function(x, ..., srcref = NULL) {
+  error <- x$message
+
+  msg <- gsub("Error.*?: ", "", as.character(error))
+  # Remove trailing newline to be consistent with other conditons
+  msg <- gsub("\n$", "", msg)
+
+  expectation("error", msg, srcref)
+}
+
+#' @export
+as.expectation.warning <- function(x, ..., srcref = NULL) {
+  expectation("warning", x$message, srcref)
+}
+
+#' @export
+as.expectation.skip <- function(x, ..., srcref = NULL) {
+  expectation("skip", x$message, srcref)
+}
+
+# expectation_type --------------------------------------------------------
+
+expectation_type <- function(exp) {
+  stopifnot(is.expectation(exp))
+  gsub("^expectation_", "", class(exp)[[1]])
+}
+
+expectation_success <- function(exp) expectation_type(exp) == "success"
+expectation_failure <- function(exp) expectation_type(exp) == "failure"
+expectation_error   <- function(exp) expectation_type(exp) == "error"
+expectation_skip    <- function(exp) expectation_type(exp) == "skip"
+expectation_warning <- function(exp) expectation_type(exp) == "warning"
+expectation_broken  <- function(exp) expectation_failure(exp) || expectation_error(exp)
+expectation_ok      <- function(exp) expectation_type(exp) %in% c("success", "warning")
 
 single_letter_summary <- function(x) {
   switch(expectation_type(x),
