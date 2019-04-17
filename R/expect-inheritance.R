badf497567590089353a6d14e754cb6574cc56f3
Hadley Wickham <h.wickham@gmail.com>
2019-04-17 08:47:22 -0500

    Improve documentation of expect_type() and friends
    Pull out expect_null() and expect_is() into separate files.
    
    Fixes #865

--- R/expect-inheritance.R
+++ R/expect-inheritance.R
@@ -1,46 +1,38 @@
-#' Expectation: does the object inherit from a S3 or S4 class, or a base type?
+#' Expectation: does the object inherit from a S3 or S4 class, or is it a base type?
 #'
-#' Tests whether or not an object inherits from any of a list of classes, or
-#' is an instance of a base type. `expect_null()` is a special case designed
-#' for detecting `NULL`.
+#' @description
+#' See <https://adv-r.hadley.nz/oo.html> for an overview of R's OO systems, and
+#' the vocabulary used here.
 #'
-#' `expect_is()` is an older form. I'd recommend using `expect_s3_class()`
-#' or `expect_s4_class()` in order to more clearly convey intent.
+#' * `expect_type(x, type)` checks that `typeof(x)` is `type`.
+#' * `expect_s3_class(x, class)` checks that `x` is an S3 object that
+#'   [inherits()] from `class`
+#' * `expect_s4_class(x, class)` checks that `x` is an S4 object that
+#'   [is()] `class`.
 #'
+#' @param type String giving base type (as returned by [typeof()]).
+#' @param class character vector of class names
 #' @inheritParams expect_that
-#' @seealso [inherits()]
 #' @family expectations
 #' @examples
-#' expect_is(1, "numeric")
-#' a <- matrix(1:10, nrow = 5)
-#' expect_is(a, "matrix")
+#' x <- data.frame(x = 1:10, y = "x")
+#' # A data frame is an S3 object with class data.frame
+#' expect_s3_class(x, "data.frame")
+#' show_failure(expect_s4_class(x, "data.frame"))
+#' # A data frame is built from a list:
+#' expect_type(x, "list")
 #'
-#' expect_is(mtcars, "data.frame")
-#' # alternatively for classes that have an is method
-#' expect_true(is.data.frame(mtcars))
+#' # An integer vector is an atomic vector of type "integer"
+#' expect_type(x$x, "integer")
+#' # It is not an S3 object
+#' show_failure(expect_s3_class(x$x, "integer"))
 #'
-#' f <- factor("a")
-#' expect_is(f, "factor")
-#' expect_s3_class(f, "factor")
-#' expect_type(f, "integer")
-#'
-#' expect_null(NULL)
-#'
+#' # By default data.frame() converts characters to factors:
+#' show_failure(expect_type(x$y, "character"))
+#' expect_s3_class(x$y, "factor")
+#' expect_type(x$y, "integer")
 #' @name inheritance-expectations
 NULL
-
-#' @export
-#' @rdname inheritance-expectations
-expect_null <- function(object, info = NULL, label = NULL) {
-  act <- quasi_label(enquo(object), label, arg = "object")
-
-  expect(
-    is.null(act$val),
-    sprintf("%s is not null.", act$lab),
-    info = info
-  )
-  invisible(act$val)
-}
 
 #' @export
 #' @rdname inheritance-expectations
@@ -53,25 +45,6 @@ expect_type <- function(object, type) {
   expect(
     identical(act_type, type),
     sprintf("%s has type `%s`, not `%s`.", act$lab, act_type, type)
-  )
-  invisible(act$val)
-}
-
-#' @param class character vector of class names
-#' @param type String giving base type (as returned by [typeof()]).
-#' @export
-#' @rdname inheritance-expectations
-expect_is <- function(object, class, info = NULL, label = NULL) {
-  stopifnot(is.character(class))
-
-  act <- quasi_label(enquo(object), label, arg = "object")
-  act$class <- klass(act$val)
-  exp_lab <- paste(class, collapse = "/")
-
-  expect(
-    inherits(act$val, class),
-    sprintf("%s inherits from `%s` not `%s`.", act$lab, act$class, exp_lab),
-    info = info
   )
   invisible(act$val)
 }
@@ -117,3 +90,28 @@ expect_s4_class <- function(object, class) {
 }
 
 isS3 <- function(x) is.object(x) && !isS4(x)
+
+#' Expectation: does the object inherit from a given class?
+#'
+#' `expect_is()` is an older form that uses [inherits()] without checking
+#' whether `x` is S3, S4, or neither. Intead, I'd recommend using
+#' [expect_type()], [expect_s3_class()] or [expect_s4_class()] to more clearly
+#' convey your  intent.
+#'
+#' @keywords internal
+#' @inheritParams expect_type
+#' @export
+expect_is <- function(object, class, info = NULL, label = NULL) {
+  stopifnot(is.character(class))
+
+  act <- quasi_label(enquo(object), label, arg = "object")
+  act$class <- klass(act$val)
+  exp_lab <- paste(class, collapse = "/")
+
+  expect(
+    inherits(act$val, class),
+    sprintf("%s inherits from `%s` not `%s`.", act$lab, act$class, exp_lab),
+    info = info
+  )
+  invisible(act$val)
+}
