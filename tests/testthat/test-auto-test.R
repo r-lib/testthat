0d84b135308f172a05e3a09cd3a5c5e2c4590cbb
CorradoLanera <corrado.lanera@gmail.com>
2019-06-29 22:42:13 -0400

    test for auto-test

---
+++ tests/testthat/test-auto-test.R
@@ -0,0 +1,41 @@
+test_that("auto_test_package_job throw expected errors", {
+  if (!requireNamespace("rstudioapi")) {
+    expect_error(auto_test_package_job(), "rstudioapi required")
+  } else if (!rstudioapi::isAvailable("1.2")) {
+    expect_error(
+      auto_test_package_job(),
+      "Need at least|RStudio not running"
+    )
+  } else {
+    expect_error(auto_test_package_job(1), "invalid 'path'")
+  }
+})
+
+
+test_that("stop_auto_test_job throw expected errors", {
+
+  if (!requireNamespace("rstudioapi")) {
+    expect_error(stop_auto_test_job(), "rstudioapi required")
+
+
+  } else if (!rstudioapi::isAvailable("1.2")) {
+    expect_error(stop_auto_test_job(),
+      "Need at least|RStudio not running"
+    )
+
+
+  } else if (can_close_jobs()) {
+    expect_error(stop_auto_test_job(1), "does not exists")
+
+  } else {
+    expect_warning(stop_auto_test_job(),
+      "jobs cannot be closed automatically"
+    )
+
+    expect_message(suppressWarnings(stop_auto_test_job()),
+      "jobs can be closed manually"
+    )
+  }
+})
+
+
