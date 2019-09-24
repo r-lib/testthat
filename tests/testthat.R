efc747ab9fdb32401c76959426a57c4798cf7fae
Lionel Henry <lionel.hry@gmail.com>
2019-09-24 10:51:46 +0200

    Revert failing `test_that()` to an error

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,9 +1,16 @@
 library(testthat)
 options(testthat.use_colours = FALSE)
 
-test_that("can run test interactively", {
+out <- test_that("running succeeding test outside of test reporter works", {
   expect_true(TRUE)
-  expect_true(FALSE)
 })
+expect_true(out)
+
+err <- rlang::catch_cnd(
+  test_that("running failing test outside of test reporter is an error", {
+    expect_true(FALSE)
+  })
+)
+expect_is(err, "error")
 
 test_check("testthat")
