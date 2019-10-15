8176d1d031c9e26902cb4bfd80356c094b624daf
Lionel Henry <lionel.hry@gmail.com>
2019-10-15 11:25:35 +0200

    Add backport for `strrep()`

--- R/utils.R
+++ R/utils.R
@@ -110,3 +110,17 @@ maybe_restart <- function(restart) {
     invokeRestart(restart)
   }
 }
+
+# Backport for R 3.2
+strrep <- function(x, times) {
+  x = as.character(x)
+  if (length(x) == 0L)
+    return(x)
+  unlist(.mapply(function(x, times) {
+    if (is.na(x) || is.na(times))
+      return(NA_character_)
+    if (times <= 0L)
+      return("")
+    paste0(replicate(times, x), collapse = "")
+  }, list(x = x, times = times), MoreArgs = list()), use.names = FALSE)
+}
