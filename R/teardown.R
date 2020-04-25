2d6f10d9f3dadc8632e3d639422fc44a3f1f8c52
Kosei Abe <abe.kosei@connecto-data.com>
2020-04-25 19:09:05 +0900

    flip the argument order instead of giving explicit parameter name

--- R/teardown.R
+++ R/teardown.R
@@ -16,7 +16,7 @@ teardown_env$queue <- list()
 #' \dontrun{
 #'
 #' tmp <- tempfile()
-#' setup(writeLines(con = tmp, "some test data"))
+#' setup(writeLines("some test data", tmp))
 #' teardown(unlink(tmp))
 #'
 #' }
