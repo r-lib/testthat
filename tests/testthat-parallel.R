fcd8c4cf2adb70ec80584de60bdd6cb614871ac1
Gábor Csárdi <csardi.gabor@gmail.com>
2020-05-15 13:07:43 +0100

    Do no run tests in parallel in covr

--- tests/testthat-parallel.R
+++ tests/testthat-parallel.R
@@ -1,6 +1,11 @@
 library(testthat)
 
-withr::with_envvar(
-  c(TESTTHAT_PARALLEL = "true"),
-  test_check("testthat")
-)
+# We don't currently do this in covr, because covr does not seem to
+# work with parallel tests.
+
+if (Sys.getenv("R_COVR", "") == "") {
+  withr::with_envvar(
+    c(TESTTHAT_PARALLEL = "true"),
+    test_check("testthat")
+  )
+}
