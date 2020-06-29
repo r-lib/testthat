4ed73975db9012b0df164f1ae8be26683d92b174
Hadley Wickham <h.wickham@gmail.com>
2020-06-29 17:20:04 -0500

    Never show praise when testing

--- tests/testthat/test-reporter-progress.R
+++ tests/testthat/test-reporter-progress.R
@@ -2,7 +2,10 @@ test_that("captures error before first test", {
   local_reproducible_output(unicode = TRUE)
 
   expect_known_output(
-    test_file(test_path("reporters/error-setup.R"), ProgressReporter$new()),
+    test_file(
+      test_path("reporters/error-setup.R"),
+      ProgressReporter$new(show_praise = FALSE)
+    ),
     test_path("reporters/progress-error-setup.txt")
   )
 })
