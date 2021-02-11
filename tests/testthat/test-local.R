8089632e4915c192b447fe405f4b183832c83f6d
Jennifer (Jenny) Bryan <jenny.f.bryan@gmail.com>
2021-02-11 05:01:22 -0800

    Docs and tests for local_* reproducibility functions (#1297)

--- tests/testthat/test-local.R
+++ tests/testthat/test-local.R
@@ -1,10 +1,28 @@
-test_that("expected options are set by default", {
+test_that("local context is 'as promised' inside test_that()", {
+  # high-level expectations or expectations that preceded testthat 3e
   expect_true(is_testing())
   expect_equal(testing_package(), "testthat")
   expect_false(is_interactive())
   expect_equal(Sys.getenv("R_TESTS"), "")
+
+  # set in local_test_context()
+  expect_equal(Sys.getenv("TESTTHAT"), "true")
+
+  # testthat 3e, set in local_reproducible_output()
   expect_equal(edition_get(), 3L)
+  expect_equal(getOption("width"), 80)
+  expect_false(getOption("crayon.enabled"))
+  expect_false(getOption("cli.dynamic"))
+  expect_false(getOption("cli.unicode"))
+  expect_equal(getOption("lifecycle_verbosity"), "warning")
+  expect_equal(getOption("OutDec"), ".")
+  expect_false(getOption("rlang_interactive"))
+  expect_false(getOption("useFancyQuotes"))
+
   expect_equal(Sys.getenv("RSTUDIO"), "")
+  expect_equal(Sys.getenv("LANGUAGE"), "en")
+
+  expect_equal(Sys.getlocale("LC_COLLATE"), "C")
 })
 
 test_that("can override usual options", {
