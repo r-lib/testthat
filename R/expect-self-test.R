5e33dfa01b1092f50546950c5992eceb736038c4
Hadley Wickham <h.wickham@gmail.com>
2021-05-04 07:02:22 -0500

    Use newer local_seed() function

--- R/expect-self-test.R
+++ R/expect-self-test.R
@@ -70,14 +70,11 @@ expect_snapshot_failure <- function(x) {
 
 expect_snapshot_reporter <- function(reporter, path = test_path("reporters/tests.R")) {
   withr::local_rng_version("3.3")
-  withr::with_seed(1014, {
-    expect_snapshot_output(
-      with_reporter(
-        reporter,
-        test_one_file(path)
-      )
-    )
-  })
+  withr::local_seed(1014)
+
+  expect_snapshot_output(
+    with_reporter(reporter, test_one_file(path))
+  )
 }
 
 # Use specifically for testthat tests in order to override the
