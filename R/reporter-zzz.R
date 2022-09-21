457420ae9a41105012faf2e394a9b7abe2a80649
Hadley Wickham <h.wickham@gmail.com>
2022-09-21 09:39:55 -0500

    Use single condition; update snapshots

--- R/reporter-zzz.R
+++ R/reporter-zzz.R
@@ -58,8 +58,11 @@ with_reporter <- function(reporter, code, start_end_reporter = TRUE) {
 }
 
 stop_reporter <- function(message) {
-  signal("", class = "testthat_abort_reporter")
-  cli::cli_abort(message, error_call = NULL)
+  cli::cli_abort(
+    message,
+    class = "testthat_abort_reporter",
+    error_call = NULL
+  )
 }
 
 #' Find reporter object given name or object.
