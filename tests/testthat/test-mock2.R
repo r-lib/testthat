51b764156bc128e57eba065265c308a76a573c08
Hadley Wickham <h.wickham@gmail.com>
2023-02-16 15:44:19 -0600

    Test that you can wrap

--- tests/testthat/test-mock2.R
+++ tests/testthat/test-mock2.R
@@ -8,6 +8,19 @@ test_that("local_mocked_bindings affects local bindings", {
   expect_equal(test_mock1(), "x")
 })
 
+test_that("can make wrapper", {
+  local_mock_x <- function(env = caller_env()) {
+    local_mocked_bindings(test_mock2 = function() "x", .env = env)
+  }
+
+  local({
+    local_mock_x()
+    expect_equal(test_mock1(), "x")
+  })
+
+  expect_equal(test_mock1(), 10)
+})
+
 test_that("with_mocked_bindings() validates its inputs", {
   expect_snapshot(error = TRUE, {
     with_mocked_bindings(1 + 1, function() 2)
