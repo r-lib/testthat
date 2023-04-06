93a910fd45ba5ac8ef112ac531471d96ee18fdb2
Hadley Wickham <h.wickham@gmail.com>
2023-04-06 14:11:41 -0500

    Update docs too

--- R/mock.R
+++ R/mock.R
@@ -3,20 +3,12 @@
 #' @description
 #' `r lifecycle::badge("superseded")`
 #'
-#' `with_mock()` and `local_mock()` are superseded in favour of the more
-#' rigorous techniques found in the [mockr](https://krlmlr.github.io/mockr/)
-#' and [mockery](https://github.com/r-lib/mockery#mockery) packages.
+#' `with_mock()` and `local_mock()` are superseded in favour of
+#' [with_mocked_bindings()] and [local_mocked_bindings()].
 #'
-#' Mocking allows you to temporary replace the implementation of functions
-#' within a package, which useful for testing code that relies on functions
-#' that are slow, have unintended side effects or access resources that may
-#' not be available when testing.
-#'
-#' This works by using some C code to temporarily modify the mocked function
-#' _in place_. On exit, all functions are restored to their previous state.
-#' This is somewhat abusive of R's internals so use with care. In particular,
-#' functions in base packages cannot be mocked; to work aroud you'll need to
-#' make a wrapper function in your own package..
+#' These works by using some C code to temporarily modify the mocked function
+#' _in place_. This is abusive of R's internals, which makes it dangerous, and
+#' no longer recommended.
 #'
 #' @section 3rd edition:
 #' `r lifecycle::badge("deprecated")`
