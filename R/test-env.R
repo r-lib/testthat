4c2d140f818f3b2fcb6441d4f78629f4590df904
Hadley Wickham <h.wickham@gmail.com>
2023-06-29 15:05:01 -0500

    Doc improvements

--- R/test-env.R
+++ R/test-env.R
@@ -2,18 +2,18 @@
 #'
 #' @description
 #' These functions help you determine if you code is running in a particular
-#' testing content:
+#' testing context:
 #'
-#' * `is_testing()` is `TRUE` when run inside a test.
-#' * `is_parallel()` is `TRUE` if the test is run in parallel.
-#' * `is_checking()` is `TRUE` when the tests run as part of `R CMD check`
-#'   (i.e. by [test_check()]).
+#' * `is_testing()` is `TRUE` inside a test.
+#' * `is_snapshot()` is `TRUE` inside a snapshot test
+#' * `is_checking()` is `TRUE` inside of `R CMD check` (i.e. by [test_check()]).
+#' * `is_parallel()` is `TRUE` if the tests are run in parallel.
 #' * `testing_package()` gives name of the package being tested.
 #'
 #' A common use of these functions is to compute a default value for a `quiet`
-#' argument with `is_testing() && !is_snapshotting()`. In this case, you'll
+#' argument with `is_testing() && !is_snapshot()`. In this case, you'll
 #' want to avoid an run-time dependency on testthat, in which case you should
-#' just copy the implementation of these functions in a `utils.R` or similar.
+#' just copy the implementation of these functions into a `utils.R` or similar.
 #'
 #' @export
 is_testing <- function() {
