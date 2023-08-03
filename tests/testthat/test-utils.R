c9c27ef20e76f90bf97010ea75eaaf1efda4d32b
Hadley Wickham <h.wickham@gmail.com>
2023-08-03 15:46:49 -0500

    Add more tests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,7 +1,7 @@
 
 # srcrefs ----------------------------------------------------------
 
-expectation_lines <- function(code) {
+srcref_line <- function(code) {
   srcref <- attr(substitute(code), "srcref")
   if (!is.list(srcref)) {
     stop("code doesn't have srcref", call. = FALSE)
@@ -11,27 +11,49 @@ expectation_lines <- function(code) {
   unlist(lapply(results, function(x) x$srcref[1])) - srcref[[1]][1]
 }
 
-test_that("line numbers captured in simple case", {
-  lines <- expectation_lines({
-                                 # line 1
-    test_that("simple", {        # line 2
-      expect_true(FALSE)         # line 3
-    })                           # line 4
+test_that("line numbers captured for expectations and warnings", {
+  f <- function() warning("Uh oh")
+  lines <- srcref_line({
+    test_that("simple", {        # line 1
+      expect_true(FALSE)         # line 2
+      f()                        # line 3
+    })
   })
-  expect_equal(lines, 3)
+  expect_equal(lines, c(2, 3))
 })
 
-test_that("line numbers captured inside another function", {
-  lines <- expectation_lines({
+test_that("line numbers captured when called indirectly", {
+  lines <- srcref_line({
     test_that("simple", {                    # line 1
       suppressMessages(expect_true(FALSE))   # line 2
     })
   })
   expect_equal(lines, 2)
+
+  lines <- srcref_line({
+    test_that("simple", {           # line 1
+      f <- function() g()           # line 2
+      g <- function() h()           # line 3
+      h <- function() expect_true() # line 4
+                                    # line 5
+      h()                           # line 6
+    })
+  })
+  expect_equal(lines, 4)
+
+  lines <- srcref_line({
+    f <- function() g()               # line 1
+    g <- function() h()               # line 2
+    h <- function() expect_true()     # line 3
+    test_that("simple", {             # line 4
+      h()                             # line 5
+    })
+  })
+  expect_equal(lines, 5)
 })
 
 test_that("line numbers captured inside a loop", {
-  lines <- expectation_lines({
+  lines <- srcref_line({
     test_that("simple", {               # line 1
       for (i in 1:4) expect_true(TRUE)  # line 2
     })
@@ -39,20 +61,36 @@ test_that("line numbers captured inside a loop", {
   expect_equal(lines, rep(2, 4))
 })
 
-test_that("line numbers captured for skip()s", {
-  lines <- expectation_lines({
+test_that("line numbers captured for skip()s and stops()", {
+  lines <- srcref_line({
     test_that("simple", {             # line 1
       skip("Not this time")           # line 2
     })                                # line 3
   })
   expect_equal(lines, 2)
+
+  lines <- srcref_line({
+    test_that("simple", {             # line 1
+      stop("Not this time")           # line 2
+    })                                # line 3
+  })
+  expect_equal(lines, 2)
+
 })
 
-test_that("line numbers captured for stop()s", {
-  lines <- expectation_lines({
+test_that("line numbers captured for on.exit()", {
+  lines <- srcref_line({
     test_that("simple", {             # line 1
-      skip("Not this time")           # line 2
+      on.exit({stop("Error")})        # line 2
     })                                # line 3
   })
   expect_equal(lines, 2)
+
+  # Falls back to test if no srcrf
+  lines <- srcref_line({
+    test_that("simple", {             # line 1
+      on.exit(stop("Error"))          # line 2
+    })                                # line 3
+  })
+  expect_equal(lines, 1)
 })
