3d4740241c204fed475be6a3357a618b5d8e5137
Kirill Müller <kirill@cynkra.com>
2023-10-09 09:15:04 +0200

    Try setting LC_ALL

--- R/local.R
+++ R/local.R
@@ -133,8 +133,11 @@ local_reproducible_output <- function(width = 80,
     .local_envir = .env
   )
 
-  if (isTRUE(capabilities("NLS")) && !(Sys.getenv("LANG") %in% c("C", "C.UTF-8"))) {
-    withr::local_language(lang, .local_envir = .env)
+  if (isTRUE(capabilities("NLS"))) {
+    if (!(Sys.getenv("LANG") %in% c("C", "C.UTF-8"))) {
+      withr::local_language(lang, .local_envir = .env)
+    }
+    withr::local_envvar(c(LC_ALL = "en_GB"), .local_envir = .env)
   }
 
   withr::local_collate("C", .local_envir = .env)
