02542084fba559e2c11fcea7e1f4ba88caee98e8
Hadley Wickham <h.wickham@gmail.com>
2024-10-22 17:10:37 -0500

    Only seems to work on recent versions

--- tests/testthat/test-parallel-crash.R
+++ tests/testthat/test-parallel-crash.R
@@ -1,6 +1,8 @@
 test_that("crash", {
   skip_on_cran()
   skip_on_covr()
+  skip_if_not(getRversion() >= "4.4.0")
+
   withr::local_envvar(TESTTHAT_PARALLEL = "TRUE")
 
   pkg <- test_path("test-parallel", "crash")
