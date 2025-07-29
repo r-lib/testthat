7539980c32f91f699d6c6ae9100f514cc3dc91fd
Hadley Wickham <h.wickham@gmail.com>
2025-07-29 12:04:09 -0500

    Add clarifying comment

--- R/test-that.R
+++ R/test-that.R
@@ -142,6 +142,7 @@ test_code <- function(test, code, env, reporter, skip_on_empty = TRUE) {
   handle_expectation <- function(e) {
     handled <<- TRUE
     register_expectation(e, 7)
+    # Don't bubble up to any other handlers
     invokeRestart("continue_test")
   }
   handle_warning <- function(e) {
