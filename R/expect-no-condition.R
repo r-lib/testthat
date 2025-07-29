889de4002ff81258eecc803e6521a0d4287a9738
Hadley Wickham <h.wickham@gmail.com>
2025-07-29 17:46:36 -0500

    Ensure `expect_no_*()` runs complete block
    Instead of terminating early. Fixes #1991.

--- R/expect-no-condition.R
+++ R/expect-no-condition.R
@@ -82,45 +82,53 @@ expect_no_ <- function(
       is.null(class)
   )
 
+  first_match <- NULL
   capture <- function(code) {
-    try_fetch(
-      {
-        code
-        # We can't call succeed() here because that generates a condition
-        # that causes `expect_no_condition()` to always fail
-        NULL
-      },
-      !!base_class := function(cnd) {
-        if (!matcher(cnd)) {
-          return(zap())
-        }
+    withRestarts(
+      withCallingHandlers(
+        code,
+        condition = function(cnd) {
+          if (!is.null(first_match) || !matcher(cnd)) {
+            return()
+          }
 
-        expected <- paste0(
-          "Expected ",
-          quo_label(enquo(object)),
-          " to run without any ",
-          base_class,
-          "s",
-          if (!is.null(class)) paste0(" of class '", class, "'"),
-          if (!is.null(regexp)) paste0(" matching pattern '", regexp, "'"),
-          "."
-        )
-        actual <- paste0(
-          "Actually got a <",
-          class(cnd)[[1]],
-          "> with text:\n",
-          indent_lines(rlang::cnd_message(cnd))
-        )
-        message <- format_error_bullets(c(expected, i = actual))
-        fail(message, trace_env = trace_env)
-      }
+          first_match <<- cnd
+          cnd_muffle(cnd)
+
+          # Matching errors should terminate execution
+          if (inherits(cnd, "error")) {
+            invokeRestart("done")
+          }
+        }
+      ),
+      done = function() {}
     )
   }
 
   act <- quasi_capture(enquo(object), NULL, capture)
-  if (is.null(act$cap)) {
-    succeed()
+
+  if (!is.null(first_match)) {
+    expected <- paste0(
+      "Expected ",
+      quo_label(enquo(object)),
+      " to run without any ",
+      base_class,
+      "s",
+      if (!is.null(class)) paste0(" of class '", class, "'"),
+      if (!is.null(regexp)) paste0(" matching pattern '", regexp, "'"),
+      "."
+    )
+    actual <- paste0(
+      "Actually got a <",
+      class(first_match)[[1]],
+      "> with text:\n",
+      indent_lines(rlang::cnd_message(first_match))
+    )
+    message <- format_error_bullets(c(expected, i = actual))
+    return(fail(message, trace_env = trace_env))
   }
+
+  succeed()
   invisible(act$val)
 }
 
