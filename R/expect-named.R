78f4a4ad3efe37157ab5775a1bbdc77877f2f0ef
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2025-07-30 20:34:15 -0700

    switch expect_named() from using expect() to pass()/fail()

--- R/expect-named.R
+++ R/expect-named.R
@@ -39,45 +39,48 @@ expect_named <- function(
     if (identical(act$names, NULL)) {
       msg <- sprintf("%s does not have names.", act$lab)
       return(fail(msg))
-    }
-  } else {
-    exp <- quasi_label(enquo(expected), arg = "expected")
-    exp$val <- normalise_names(exp$val, ignore.order, ignore.case)
-    act$names <- normalise_names(act$names, ignore.order, ignore.case)
-
-    if (ignore.order) {
-      act_miss <- unique(act$names[!act$names %in% exp$val])
-      exp_miss <- unique(exp$val[!exp$val %in% act$names])
-
-      expect(
-        length(exp_miss) == 0 && length(act_miss) == 0,
-        paste0(
-          "Names of ",
-          act$lab,
-          " (`actual`) and ",
-          exp$lab,
-          " (`expected`) don't have the same values.\n",
-          if (length(act_miss)) {
-            paste0("* Only in `actual`: ", values(act_miss), "\n")
-          },
-          if (length(exp_miss)) {
-            paste0("* Only in `expected`: ", values(exp_miss), "\n")
-          }
-        )
-      )
     } else {
-      expect(
-        identical(act$names, exp$val),
-        sprintf(
-          "Names of %s (%s) don't match %s",
-          act$lab,
-          paste0("'", act$names, "'", collapse = ", "),
-          paste0("'", exp$val, "'", collapse = ", ")
-        ),
-        info = info
-      )
+      return(pass(act$val))
     }
   }
+
+  exp <- quasi_label(enquo(expected), arg = "expected")
+
+  exp$val <- normalise_names(exp$val, ignore.order, ignore.case)
+  act$names <- normalise_names(act$names, ignore.order, ignore.case)
+
+  if (ignore.order) {
+    act_miss <- unique(act$names[!act$names %in% exp$val])
+    exp_miss <- unique(exp$val[!exp$val %in% act$names])
+
+    if (length(exp_miss) != 0 || length(act_miss) != 0) {
+      msg <- paste0(
+        "Names of ",
+        act$lab,
+        " (`actual`) and ",
+        exp$lab,
+        " (`expected`) don't have the same values.\n",
+        if (length(act_miss)) {
+          paste0("* Only in `actual`: ", values(act_miss), "\n")
+        },
+        if (length(exp_miss)) {
+          paste0("* Only in `expected`: ", values(exp_miss), "\n")
+        }
+      )
+      return(fail(msg))
+    }
+  } else {
+    if (!identical(act$names, exp$val)) {
+      msg <- sprintf(
+        "Names of %s (%s) don't match %s",
+        act$lab,
+        paste0("'", act$names, "'", collapse = ", "),
+        paste0("'", exp$val, "'", collapse = ", ")
+      )
+      return(fail(msg))
+    }
+  }
+
   pass(act$val)
 }
 
