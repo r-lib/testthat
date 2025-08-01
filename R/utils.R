cf999fa0eedcf2a7f07e78822c1a3cfdbd451a85
Hadley Wickham <h.wickham@gmail.com>
2025-08-01 08:52:49 -0500

    Use stanadlone purrr file

--- R/utils.R
+++ R/utils.R
@@ -14,10 +14,6 @@ transport_fun <- function(f) {
   f
 }
 
-compact <- function(x) {
-  x[lengths(x) > 0]
-}
-
 # Handled specially in test_code so no backtrace
 testthat_warn <- function(message, ...) {
   warn(message, class = "testthat_warn", ...)
@@ -51,15 +47,6 @@ first_upper <- function(x) {
 
 in_rcmd_check <- function() {
   nzchar(Sys.getenv("_R_CHECK_PACKAGE_NAME_", ""))
-}
-
-map_chr <- function(.x, .f, ...) {
-  .f <- as_function(.f)
-  vapply(.x, .f, FUN.VALUE = character(1), ...)
-}
-map_lgl <- function(.x, .f, ...) {
-  .f <- as_function(.f)
-  vapply(.x, .f, FUN.VALUE = logical(1), ...)
 }
 
 r_version <- function() paste0("R", getRversion()[, 1:2])
