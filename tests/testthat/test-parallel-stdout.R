8221f82ecfe5b44c75d45d3fa833bb6b457a0087
Gábor Csárdi <csardi.gabor@gmail.com>
2025-08-06 17:11:43 +0200

    Better test for parallel output

--- tests/testthat/test-parallel-stdout.R
+++ tests/testthat/test-parallel-stdout.R
@@ -5,13 +5,15 @@ test_that("stdout/stderr in parallel code", {
     test_path("test-parallel", "stdout"),
     reporter = "summary"
   )))
-  expect_true("> test-stdout-2.R: This is a message!" %in% out)
-  expect_true(any(grepl("test-stdout-3.R:  [1]  1  2  3", out, fixed = TRUE)))
+  out <- paste(out, collapse = "\n")
+  expect_match(out, "> test-stdout-2.R: This is a message!", fixed = TRUE)
+  expect_match(out, "test-stdout-3.R:  [1]  1  2  3", fixed = TRUE)
 
   out2 <- capture.output(suppressMessages(testthat::test_local(
     test_path("test-parallel", "stdout"),
     reporter = "progress"
   )))
-  expect_true("> test-stdout-2.R: This is a message!" %in% out2)
-  expect_true(any(grepl("test-stdout-3.R:  [1]  1  2  3", out2, fixed = TRUE)))
+  out2 <- paste(out2, collapse = "\n")
+  expect_match(out2, "> test-stdout-2.R: This is a message!", fixed = TRUE)
+  expect_match(out2, "test-stdout-3.R:  [1]  1  2  3", fixed = TRUE)
 })
