ec562c9efd3cb9ed7eccff2e1a703bfbe3e9e891
Gábor Csárdi <csardi.gabor@gmail.com>
2025-08-07 09:07:54 +0200

    Better way to use expect_match()

--- tests/testthat/test-parallel-stdout.R
+++ tests/testthat/test-parallel-stdout.R
@@ -5,15 +5,28 @@ test_that("stdout/stderr in parallel code", {
     test_path("test-parallel", "stdout"),
     reporter = "summary"
   )))
-  out <- paste(out, collapse = "\n")
-  expect_match(out, "> test-stdout-2.R: This is a message!", fixed = TRUE)
-  expect_match(out, "test-stdout-3.R:  [1]  1  2  3", fixed = TRUE)
+  expect_match(
+    out,
+    "> test-stdout-2.R: This is a message!",
+    fixed = TRUE,
+    all = FALSE
+  )
+  expect_match(out, "test-stdout-3.R:  [1]  1  2  3", fixed = TRUE, all = FALSE)
 
   out2 <- capture.output(suppressMessages(testthat::test_local(
     test_path("test-parallel", "stdout"),
     reporter = "progress"
   )))
-  out2 <- paste(out2, collapse = "\n")
-  expect_match(out2, "> test-stdout-2.R: This is a message!", fixed = TRUE)
-  expect_match(out2, "test-stdout-3.R:  [1]  1  2  3", fixed = TRUE)
+  expect_match(
+    out2,
+    "> test-stdout-2.R: This is a message!",
+    fixed = TRUE,
+    all = FALSE
+  )
+  expect_match(
+    out2,
+    "test-stdout-3.R:  [1]  1  2  3",
+    fixed = TRUE,
+    all = FALSE
+  )
 })
