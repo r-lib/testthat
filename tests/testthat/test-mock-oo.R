66ddd0c4d8554048429276809f45c9d77a164530
Hadley Wickham <h.wickham@gmail.com>
2026-01-07 13:56:53 -0600

    Enhance OO mocking (#2303)
    Make it possible to mock methods that don't exist, and make it possible to mock the absence of a method.
    
    Fixes #2302

--- tests/testthat/test-mock-oo.R
+++ tests/testthat/test-mock-oo.R
@@ -16,8 +16,34 @@ test_that("validates its inputs", {
     local_mocked_s3_method(1)
     local_mocked_s3_method("mean", 1)
     local_mocked_s3_method("mean", "bar", 1)
-    local_mocked_s3_method("mean", "bar", function() {})
+    local_mocked_s3_method("notAGeneric", "bar", function() {})
   })
+})
+
+test_that("can mock S3 method that doesn't exist yet", {
+  x <- structure(list(), class = "test_mock_class")
+
+  local({
+    local_mocked_s3_method("length", "test_mock_class", function(x) 42)
+    expect_length(x, 42)
+  })
+
+  # Method should be removed after scope ends
+  expect_length(x, 0)
+})
+
+test_that("can temporarily remove S3 method with NULL", {
+  x <- structure(list(), class = "test_mock_class2")
+  local_mocked_s3_method("length", "test_mock_class2", function(x) 42)
+
+  local({
+    # Now remove it
+    local_mocked_s3_method("length", "test_mock_class2", NULL)
+    expect_length(x, 0)
+  })
+
+  # Method should be restored after scope ends
+  expect_length(x, 42)
 })
 
 # S4 --------------------------------------------------------------------------
@@ -33,14 +59,39 @@ test_that("can mock S4 methods", {
   expect_equal(mock_age(jim), 32)
 })
 
-
 test_that("validates its inputs", {
   expect_snapshot(error = TRUE, {
     local_mocked_s4_method(1)
     local_mocked_s4_method("mean", 1)
     local_mocked_s4_method("mean", "bar", 1)
-    local_mocked_s4_method("mean", "bar", function() {})
+    local_mocked_s4_method("notAGeneric", "bar", function() {})
   })
+})
+
+test_that("can mock S4 method that doesn't exist yet", {
+  jim <- TestMockPerson(name = "Jim", age = 32)
+
+  local({
+    local_mocked_s4_method("show", "TestMockPerson", function(object) {
+      cat("Person:", object@name, "\n")
+    })
+    expect_output(show(jim), "Person: Jim")
+  })
+
+  expect_null(methods::getMethod("show", "TestMockPerson", optional = TRUE))
+})
+
+test_that("can temporarily remove S4 method with NULL", {
+  jim <- TestMockPerson(name = "Jim", age = 32)
+  expect_equal(mock_age(jim), 32)
+
+  local({
+    local_mocked_s4_method("mock_age", "TestMockPerson", NULL)
+    # Method is removed, so this should error
+    expect_error(mock_age(jim), "unable to find")
+  })
+
+  expect_equal(mock_age(jim), 32)
 })
 
 # R6 --------------------------------------------------------------------------
