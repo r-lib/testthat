b00bdf83cb54c7924f20ef6aafc3ae6b9c1a39b1
chiricom <chiricom@google.com>
2026-08-13 18:50:04 +0000

    Gemini: add tests

--- tests/testthat/test-expect-shape.R
+++ tests/testthat/test-expect-shape.R
@@ -120,3 +120,15 @@ test_that("checks inputs arguments, ", {
     expect_shape(array(1), dim = "x")
   })
 })
+
+test_that("evaluates object only once", {
+  i <- 0L
+  expect_success(expect_shape({ i <- i + 1L; matrix(nrow = 2, ncol = 2) }, dim = c(2L, 2L)))
+  expect_identical(i, 1L)
+
+  expect_warning(
+    expect_shape({ warning("test"); matrix(nrow = 2, ncol = 2) }, dim = c(2L, 2L)),
+    "test"
+  )
+})
+
